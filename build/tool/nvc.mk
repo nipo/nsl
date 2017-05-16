efe81beb9180802c56773e287f16832a92e01a56
Nicolas Pouillon <nipo@ssji.net>
2017-05-17 00:25:54 +0200

    Add support for NVC
    This is mostly broken for now, NVC does not support scoped module
    instanciations, and chokes on most file IO primitives.

---
+++ build/tool/nvc.mk
@@ -0,0 +1,37 @@
+VHDL_VERSION=1993
+NVC=nvc
+target ?= $(top)
+
+upper = $(shell echo "$1" | tr a-z A-Z)
+elab_target := work/_WORK.$(call upper,$(target)).final.bc
+
+simulate: $(target).lxt
+
+.PRECIOUS: $(target).lxt
+
+.PHONY: FORCE
+
+$(target).lxt: $(elab_target) FORCE
+	@$(NVC) -r $(top) --wave=$@
+
+$(elab_target): $(foreach l,$(libraries),$l/_NVC_LIB)
+	@$(NVC) -L. -e $(top)
+
+define nvc_source_do
+@$$(NVC) -L. --std=$$(VHDL_VERSION) --work=$2 $1 $3 > /dev/null
+	
+endef
+
+define nvc_library
+
+$1/_NVC_LIB: $($1-vhdl-sources)
+	rm -f $$
+	$(foreach s,$($1-vhdl-sources),$(call nvc_source_do,-a,$1,$s))
+
+clean-dirs += $1/
+
+endef
+
+clean-files += $(target) $(target).lxt $(target).vcd
+
+$(eval $(foreach l,$(libraries),$(call nvc_library,$l)))
