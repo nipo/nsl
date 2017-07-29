22357a44da65c02d34fe320df1968a50c1578fc6
Nicolas Pouillon <nipo@ssji.net>
2017-07-29 15:56:33 +0200

    Rework nsl.uart package

--- lib/nsl/uart/uart.vhd
+++ lib/nsl/uart/uart.vhd
@@ -3,17 +3,14 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 library nsl;
-use nsl.flit.all;
-use nsl.noc.all;
+use nsl.framed.all;
 
 package uart is
 
-  component uart_noc_8n1 is
+  component uart_framed_8n1 is
     generic(
       p_clk_rate : natural;
-      baud_rate  : natural;
-      srcid      : nsl.noc.noc_id;
-      tgtid      : nsl.noc.noc_id
+      baud_rate  : natural
       );
     port(
       p_resetn    : in std_ulogic;
@@ -22,11 +19,11 @@ package uart is
       p_uart_tx   : out std_ulogic;
       p_uart_rx   : in  std_ulogic;
 
-      p_tx_val  : in  nsl.flit.flit_cmd;
-      p_tx_ack  : out nsl.flit.flit_ack;
+      p_tx_val  : in  nsl.framed.framed_req;
+      p_tx_ack  : out nsl.framed.framed_ack;
 
-      p_rx_val : out nsl.flit.flit_cmd;
-      p_rx_ack : in  nsl.flit.flit_ack
+      p_rx_val : out nsl.framed.framed_req;
+      p_rx_ack : in  nsl.framed.framed_ack
       );
   end component;
 
