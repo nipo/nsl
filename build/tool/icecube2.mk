3a0adb95b2ba484da792ef743d979582be197fbf
Nicolas Pouillon <nipo@ssji.net>
2017-08-18 10:44:21 +0200

    tool: Add icecube2 tool backend

---
+++ build/tool/icecube2.mk
@@ -0,0 +1,158 @@
+ICECUBE2 = /opt/iCEcube2.2017.01
+SBT = $(ICECUBE2)/sbt_backend
+SBT_OPT_BIN = $(SBT)/bin/linux/opt
+SBT_OPT_LIB = $(SBT)/lib/linux/opt
+DEVICES_DIR = $(SBT)/devices
+SYNTHESIS_TOOL ?= synplify
+
+build-dir := $(target)-build
+
+empty :=
+space := $(empty) $(empty)
+comma := ,
+
+define ICECUBE2_PREPARE
+LD_LIBRARY_PATH=$(SBT_OPT_BIN)/synpwrap:$(SBT_OPT_LIB) \
+SYNPLIFY_PATH=$(ICECUBE2)/synpbase \
+TCL_LIBRARY=$(SBT)/bin/linux/lib/tcl8.4
+endef
+
+target ?= $(top)
+user_id := $(shell python3 -c 'import random ; print(hex(random.randint(0, 1<<32)))')
+
+SHELL=/bin/bash
+
+all: $(target).bin
+
+define syn_source_do
+	echo 'add_file -$($1-language) -lib $($1-library) "$1"' >> $(target)-build/synth.prj
+	$(SILENT)
+endef
+
+# Synthesis
+# Generate synplify input project
+# Run synplify
+# Converting result EDIF to SDC
+$(build-dir)/synth/$(target).sdc: $(sources) $(MAKEFILE_LIST)
+	$(SILENT)mkdir -p $(dir $@)
+	$(SILENT)> $(build-dir)/synth.prj
+	$(SILENT)$(foreach s,$(sources),$(call syn_source_do,$s))
+	$(SILENT)echo "impl -add synth -type fpga" >> $(build-dir)/synth.prj
+#	$(SILENT)echo "set_option -vhdl2008 1" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -technology SBTiCE40" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -part $(target_part)" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -package $(target_package)" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -speed_grade $(target_speed)" >> $(build-dir)/synth.prj
+#	$(SILENT)echo "set_option -speed_part_companion \"\"" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -top_module $(top-lib).$(top-entity)" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -frequency auto" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -write_verilog 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -write_vhdl 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -maxfan 10000" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -disable_io_insertion 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -pipe 1" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -retiming 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -update_models_cp 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -fixgatedclocks 2" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -fixgeneratedclocks 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -popfeed 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -constprop 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -createhierarchy 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -symbolic_fsm_compiler 1" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -compiler_compatible 0" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -resource_sharing 1" >> $(build-dir)/synth.prj
+	$(SILENT)echo "set_option -write_apr_constraint 1" >> $(build-dir)/synth.prj
+	$(SILENT)echo "project -result_format edif" >> $(build-dir)/synth.prj
+	$(SILENT)echo "project -result_file synth/$(notdir $@)" >> $(build-dir)/synth.prj
+	$(SILENT)echo "project -log_file \"synth.log\"" >> $(build-dir)/synth.prj
+	$(SILENT)echo "impl -active synth" >> $(build-dir)/synth.prj
+	$(SILENT)echo "project -run synthesis -clean" >> $(build-dir)/synth.prj
+	$(SILENT)$(ICECUBE2_PREPARE) \
+		$(SBT_OPT_BIN)/synpwrap/synpwrap \
+		-prj $(build-dir)/synth.prj
+	$(SILENT)mkdir -p $(build-dir)/synth/oadb-$(top-entity)
+	$(SILENT)$(ICECUBE2_PREPARE) \
+	$(SBT_OPT_BIN)/edifparser \
+		$(DEVICES_DIR)/$(target_dev).dev \
+		$(build-dir)/synth/$(target).edf \
+		$(build-dir)/synth \
+		-p$(target_package) \
+		-y$(subst $(space),$(comma),$(constraints)) \
+		-c \
+		--devicename $(target_part)
+	$(SILENT)cp $(build-dir)/Temp/sbt_temp.sdc $@
+
+# Place with constraints
+$(build-dir)/placed/$(target).sdc: $(build-dir)/synth/$(target).sdc
+	$(SILENT)mkdir -p $(dir $@)
+	$(SILENT)$(ICECUBE2_PREPARE) \
+	$(SBT_OPT_BIN)/sbtplacer \
+		--des-lib $(build-dir)/synth/oadb-$(top-entity) \
+		--outdir $(build-dir)/placed \
+		--device-file $(DEVICES_DIR)/$(target_dev).dev \
+		--package $(target_package) \
+		--deviceMarketName $(target_part) \
+		--sdc-file $< \
+		--lib-file $(DEVICES_DIR)/$(target_lib).lib \
+		--effort_level std \
+		--out-sdc-file $@
+
+# Router
+# Pack router input file
+# Route resulting file
+$(build-dir)/routed/$(target).sdc: $(build-dir)/placed/$(target).sdc
+	$(SILENT)mkdir -p $(dir $@)
+	$(SILENT)mkdir -p $(dir $@)/packed
+	$(SILENT)$(ICECUBE2_PREPARE) \
+		$(SBT_OPT_BIN)/packer \
+		$(DEVICES_DIR)/$(target_dev).dev \
+		$(build-dir)/synth/oadb-$(top-entity) \
+		--package $(target_package) \
+		--outdir $(dir $@) \
+		--translator $(SBT)/bin/sdc_translator.tcl \
+		--src_sdc_file $< \
+		--dst_sdc_file $(dir $@)/packed/$(notdir $@) \
+		--devicename $(target_part)
+	$(SILENT)$(ICECUBE2_PREPARE) \
+		$(SBT_OPT_BIN)/sbrouter \
+		$(DEVICES_DIR)/$(target_dev).dev \
+		$(build-dir)/synth/oadb-$(top-entity) \
+		$(DEVICES_DIR)/$(target_lib).lib \
+		$(dir $@)/packed/$(notdir $@) \
+		--outdir $(dir $@) \
+		--sdf_file $(dir $@)/routed.sdf \
+		--pin_permutation
+	$(SILENT)$(ICECUBE2_PREPARE) \
+		$(SBT_OPT_BIN)/netlister \
+		--lib $(build-dir)/synth/oadb-$(top-entity) \
+		--verilog $(@:.sdc=.v) \
+		--vhdl $(@:.sdc=.vhd) \
+		--view rt \
+		--device $(DEVICES_DIR)/$(target_dev).dev \
+		--splitio \
+		--in-sdc-file  \
+		--out-sdc-file $(dir $@)/$(target).sdc
+	$(SILENT)$(ICECUBE2_PREPARE) \
+		$(SBT_OPT_BIN)/sbtimer \
+		--des-lib $(build-dir)/synth/oadb-$(top-entity) \
+		--lib-file $(DEVICES_DIR)/$(target_lib).lib \
+		--sdc-file $(dir $@)/$(target).sdc \
+		--sdf-file $(dir $@)/routed.sdf \
+		--report-file $(dir $@)/timing.rpt \
+		--device-file $(DEVICES_DIR)/$(target_dev).dev \
+		--timing-summary
+
+# Bitstream
+$(target).bin: $(build-dir)/routed/$(target).sdc
+	$(SILENT)$(ICECUBE2_PREPARE) \
+		$(SBT_OPT_BIN)/bitmap \
+		$(DEVICES_DIR)/$(target_dev).dev \
+		--design $(build-dir)/synth/oadb-$(top-entity) \
+		--device_name $(target_part) \
+		--package $(target_package) \
+		--outdir ./ \
+		--low_power on \
+		--init_ram on \
+		--init_ram_bank 1111 \
+		--frequency low \
+		--warm_boot on
