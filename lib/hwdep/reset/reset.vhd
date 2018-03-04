05f825e5c83dbc2ae92d328330cfa94ada7091fc
Nicolas Pouillon <nipo@ssji.net>
2018-03-04 07:59:56 +0100

    hwdep: Add reset_at_startup cell

---
+++ lib/hwdep/reset/reset.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package reset is
+
+  component reset_at_startup is
+    port(
+      p_resetn    : out std_ulogic
+      );
+  end component;
+
+end package reset;
---
+++ lib/hwdep/reset/reset_at_startup_xc.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+library unisim;
+
+entity reset_at_startup is
+  port(
+    p_resetn    : out std_ulogic
+    );
+end entity;
+
+architecture xc of reset_at_startup is
+
+  signal reset: std_ulogic;
+
+begin
+
+  xc_roc: unisim.vcomponents.roc
+    port map(
+      o => reset
+      );
+
+  p_resetn <= not reset;
+  
+end;
