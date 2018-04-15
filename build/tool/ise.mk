55a5ace8fb916c3317c2027ec6b889baf68f4e25
Nicolas Pouillon <nipo@ssji.net>
2018-04-15 13:56:00 +0200

    ise.mk: Optimize build process parameters to get max speed

--- build/tool/ise.mk
+++ build/tool/ise.mk
@@ -1,9 +1,23 @@
+INTF_STYLE = -intstyle silent
+
+MAP_PLACE_ROUTE_OPTS = # -ol high -xe c
+
+MAP_OPTS        = $(MAP_PLACE_ROUTE_OPTS) -mt on
+PAR_OPTS        = $(MAP_PLACE_ROUTE_OPTS)
+#MAP_OPTS        += -global_opt speed -retiming on
+#MAP_OPTS        += -register_duplication on
+#MAP_OPTS        += -equivalent_register_removal off
+
+# They should be the same, anyway
+MAP_OPTS_GUIDED = $(MAP_OPTS)
+PAR_OPTS_GUIDED = $(PAR_OPTS)
+
 ISE = /opt/Xilinx/14.7/ISE_DS
-#INTF_STYLE = -intstyle silent
-PAR_OPTS = -ol high
-ISE_PREPARE = source $(ISE)/settings64.sh > /dev/null
+I = source $(ISE)/settings64.sh > /dev/null
 target ?= $(top)
 user_id := $(shell python3 -c 'import random ; print(hex(random.randint(0, 1<<32)))')
+
+export I
 
 SHELL=/bin/bash
 
@@ -26,23 +40,16 @@ spi-flash: $(target)-2.mcs
 	$(SILENT)CABLEDB=$(HOME)/projects/proby/support/xc3sprog/cablelist.txt \
 	xc3sprog -v -p 2 -c proby -I$(NOPROB_ROOT)/fpga/bscan_spi.bit $(<):W:0:MCS
 
-$(target).mcs: $(target).bit
-	$(ISE_PREPARE) ; \
-	promgen -spi -w -p mcs -o $@ -u 0 $<
+%.mcs: %.bit
+	$I; promgen -spi -w -p mcs -o $@ -u 0 $<
 
 clean-files += $(target).mcs
-
-$(target)-2.mcs: ise-build/$(target)-2.bit
-	$(ISE_PREPARE) ; \
-	promgen -spi -w -p mcs -o $@ -u 0 $<
-
 clean-files += $(target)-2.mcs
 clean-files += $(target)-2.cfi
 clean-files += $(target)-2.prm
 
-$(target).bit: ise-build/$(target)_par.ncd
-	$(SILENT)$(ISE_PREPARE) ; \
-	bitgen $(INTF_STYLE) \
+%.bit: ise-build/%-par.ncd
+	$(SILENT)$I; bitgen $(INTF_STYLE) \
 	    -g DriveDone:yes \
 	    -g unusedpin:pullnone \
 	    -g UserID:$(user_id) \
@@ -52,9 +59,8 @@ $(target).bit: ise-build/$(target)_par.ncd
 
 clean-files += $(target).bit
 
-$(target)-compressed.bit: ise-build/$(target)_par.ncd
-	$(SILENT)$(ISE_PREPARE) ; \
-	bitgen $(INTF_STYLE) \
+%-compressed.bit: ise-build/%-par.ncd
+	$(SILENT)$I; bitgen $(INTF_STYLE) \
 	    -g DriveDone:yes \
 	    -g unusedpin:pullnone \
 	    -g compress \
@@ -65,9 +71,8 @@ $(target)-compressed.bit: ise-build/$(target)_par.ncd
 
 clean-files += $(target)-compressed.bit
 
-ise-build/$(target)-2.bit: ise-build/$(target)_par.ncd
-	$(SILENT)$(ISE_PREPARE) ; \
-	bitgen $(INTF_STYLE) \
+ise-build/%-2.bit: ise-build/%-par.ncd
+	$(SILENT)$I; bitgen $(INTF_STYLE) \
 	    -g spi_buswidth:2 \
 	    -g unusedpin:pullnone \
 	    -g ConfigRate:26 \
@@ -80,67 +85,73 @@ ise-build/$(target)-2.bit: ise-build/$(target)_par.ncd
 clean-dirs += ise-build _xmsgs xlnx_auto_0_xdb
 
 %.mcs: %.bit
-	$(SILENT)$(ISE_PREPARE) ; \
-	promgen $(INTF_STYLE) -w -p mcs -spi -c FF -o $@ -u 0 $<
+	$(SILENT)$I; promgen $(INTF_STYLE) -w -p mcs -spi -c FF -o $@ -u 0 $<
 
-ise-build/$(target)_par.ncd: ise-build/$(target).ncd
-	$(SILENT)$(ISE_PREPARE) ; \
-	par $(INTF_STYLE) $(PAR_OPTS) -w $< $@
+ise-build/$(target)-first-map.ncd ise-build/$(target)-map.ncd:
+	$I; map $(INTF_STYLE) -p $(target_part)$(target_package)$(target_speed) \
+		$(if $(filter %-par.ncd,$^),$(MAP_OPTS_GUIDED),$(MAP_OPTS)) \
+		$(foreach g,$(filter %-par.ncd,$^),-smartguide "$g") \
+		-w "$(filter %.ngd,$^)" -o "$@"
 
-ise-build/$(target).ncd: ise-build/$(target).ngd
-	$(SILENT)if [ -r ise-build/$(target)_par.ncd ]; then \
-		cp ise-build/$(target)_par.ncd ise-build/smartguide.ncd; \
-		SMARTGUIDE="-smartguide ise-build/smartguide.ncd -detail"; \
-	else \
-		SMARTGUIDE=""; \
-	fi; \
-	$(ISE_PREPARE) ; \
-	map $(INTF_STYLE) $(MAP_OPTS) $${SMARTGUIDE} -w $<
+ise-build/$(target)-first-par.ncd ise-build/$(target)-par.ncd:
+	$(SILENT)$I; par $(INTF_STYLE) \
+		$(if $(filter %-par.ncd,$^),$(PAR_OPTS_GUIDED),$(PAR_OPTS)) \
+		$(foreach g,$(filter %-par.ncd,$^),-smartguide "$g") \
+		-w "$(filter %-map.ncd,$^)" "$@"
+
+ise-build/$(target)-first-map.ncd: ise-build/$(target).ngd
+ise-build/$(target)-first-par.ncd: ise-build/$(target)-first-map.ncd
+ise-build/$(target)-map.ncd: ise-build/$(target).ngd ise-build/$(target)-first-par.ncd
+ise-build/$(target)-par.ncd: ise-build/$(target)-map.ncd ise-build/$(target)-first-par.ncd
+
+define arg_add
+\$(empty_variable)
+    $1 $2
+endef
 
 ise-build/$(target).ngd: ise-build/$(target).ngc $(constraints)
-	$(SILENT)echo "//" > ise-build/$(target).bmm
-	$(SILENT)$(ISE_PREPARE) ; \
-	ngdbuild -dd ise-build \
-	    $(INTF_STYLE) ise-build/$(target).ngc \
-		$(foreach c,$(constraints),-uc $c) \
-	    -bm ise-build/$(target).bmm \
+	$(SILENT)echo "//" > $(@:.ngd=.bmm)
+	$(SILENT)$I; ngdbuild $(INTF_STYLE) -quiet -dd ise-build \
+	    $(foreach c,$(filter %.ngc,$^),$(call arg_add,$c)) \
+	    $(foreach c,$(filter %.ucf,$^),$(call arg_add,-uc,$c)) \
+	    -bm $(@:.ngd=.bmm) \
 	    $@
 
-ise-build/$(target).ngc: $(foreach l,$(libraries),$($l-vhdl-sources)) ise-build/$(target).xst ise-build/$(target).prj
-	$(ISE_PREPARE) ; \
-	xst $(INTF_STYLE) -ifn ise-build/$(target).xst
+%-map.pcf: %-map.ncd
+	@
+
+.PRECIOUS: %-map.pcf
 
 define ise_source_do
-	echo "$($1-language) $($1-library) $1" >> $@.tmp
+	echo "$($1-language) $($1-library) $1" >> $@.prj
 	$(SILENT)
 endef
 
-ise-build/$(target).prj: $(sources) $(MAKEFILE_LIST)
+define file_append
+	$(SILENT)cat $1 >> $2
+
+endef
+
+ise-build/$(target).ngc: $(foreach l,$(libraries),$($l-vhdl-sources)) $(sources) $(OPTS)
 	$(SILENT)mkdir -p ise-build/xst
-	$(SILENT)> $@.tmp
+	$(SILENT)> $@.prj
 	$(SILENT)$(foreach s,$(sources),$(call ise_source_do,$s))
-	$(SILENT)mv -f $@.tmp $@
 
-ise-build/$(target).xst: ise-build/$(target).prj $(OPTS) $(MAKEFILE_LIST)
-	$(SILENT)echo 'set -tmpdir "ise-build/xst"' > $@
-	$(SILENT)echo 'set -xsthdpdir "ise-build"' >> $@
-	$(SILENT)echo "run" >> $@
-	$(SILENT)echo "-p $(target_part)$(target_package)$(target_speed)" >> $@
-	$(SILENT)echo "-top $(top-entity)" >> $@
-	$(SILENT)echo "-ifn ise-build/$(target).prj" >> $@
-	$(SILENT)echo "-ofn ise-build/$(target).ngc" >> $@
-	$(SILENT)for o in $(OPTS) ; do \
-	    cat $$o >> $@ ; \
-	done
+	$(SILENT)echo 'set -tmpdir "ise-build/xst"' > $@.xst
+	$(SILENT)echo 'set -xsthdpdir "ise-build"' >> $@.xst
+	$(SILENT)echo "run" >> $@.xst
+	$(SILENT)echo "-p $(target_part)$(target_package)$(target_speed)" >> $@.xst
+	$(SILENT)echo "-top $(top-entity)" >> $@.xst
+	$(SILENT)echo "-ifn $@.prj" >> $@.xst
+	$(SILENT)echo "-ofn $@" >> $@.xst
+	$(SILENT)echo "-max_fanout 15" >> $@.xst
+	$(SILENT)echo "-read_cores yes" >> $@.xst
+#	$(SILENT)echo "-lc No" >> $@.xst
+	$(SILENT)echo "-equivalent_register_removal no" >> $@.xst
+#	$(SILENT)echo "-register_balancing yes" >> $@.xst
+	$(foreach f,$(OPS),$(call file_append,$f,$@.xst))
 
-ise-build/$(target).post_map.twr: ise-build/$(target).ncd ise-build/$(target).pcf
-	$(SILENT)$(ISE_PREPARE) ; \
-	trce -e 10 $< ise-build/$(target).pcf -o $@
+	$I; xst $(INTF_STYLE) -ifn $@.xst
 
-ise-build/$(target).twr: ise-build/$(target)_par.ncd
-	$(SILENT)$(ISE_PREPARE) ; \
-	trce $< ise-build/$(target).pcf -o $@
-
-ise-build/$(target)_err.twr: ise-build/$(target)_par.ncd
-	$(SILENT)$(ISE_PREPARE) ; \
-	trce -e 10 $< ise-build/$(target).pcf -o $@
+ise-build/%.twr: ise-build/%-par.ncd ise-build/%-map.pcf
+	$(SILENT)$I; trce -e 10 $(filter %.ncd,$^) $(filter %.pcf,$^) -o $@
