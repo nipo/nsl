f723f36efba43f15d62fdd663f6e0c0906e0b72a
Nicolas Pouillon <nipo@ssji.net>
2019-02-25 07:20:45 +0100

    Add axis signals

---
+++ lib/signalling/axis/axis.vhd
@@ -0,0 +1,29 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package axis is
+
+  type axis_16l_ms is
+  record
+    tdata  : std_ulogic_vector (15 downto 0);
+    tvalid : std_ulogic;
+    tlast  : std_ulogic;
+  end record;
+
+  type axis_16l_sm is
+  record
+    tready : std_ulogic;
+  end record;
+
+  type axis_16l_sm_vector is array(natural range <>) of axis_16l_sm;
+  type axis_16l_ms_vector is array(natural range <>) of axis_16l_ms;
+
+  type axis_16l is
+  record
+    m2s: axis_16l_ms;
+    s2m: axis_16l_sm;
+  end record;
+
+  type axis_16l_vector is array(natural range <>) of axis_16l;
+
+end package;
