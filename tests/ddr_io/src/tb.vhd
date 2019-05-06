7207ca14362b80f0984f43ddde9a7e9328f57b7d
Nicolas Pouillon <nipo@ssji.net>
2019-05-06 12:17:01 +0200

    tests/ddr_io: Fix test bench, make it work with Lattice cells

--- tests/ddr_io/src/tb.vhd
+++ tests/ddr_io/src/tb.vhd
@@ -12,87 +12,98 @@ architecture arch of tb is
   constant data_width : integer := 2;
   subtype word_t is std_ulogic_vector(data_width-1 downto 0);
   
-  signal a_clk   : std_ulogic;
-  signal io_a_d   : std_ulogic;
-  signal io_b_d   : std_ulogic;
-  signal io_a_clk : std_ulogic;
-  signal io_b_clk : std_ulogic;
-  signal a_clk_diff, b_clk90 : signalling.diff.diff_pair;
-  signal b_clk   : std_ulogic;
+  signal clk   : std_ulogic;
+  signal io_a_d, io_b_d, io_clk   : std_ulogic;
+  signal a_clk, b_clk : signalling.diff.diff_pair;
 
-  signal a_d : word_t := (others => '0');
-  signal b_d : word_t := (others => '0');
+  signal a_d, b_d : word_t := (others => '0');
 
   constant c_pattern0 : std_ulogic_vector(8 downto 0) := "000101000";
   constant c_pattern1 : std_ulogic_vector(8 downto 0) := "010100010";
+
+  signal pat : word_t := (others => '0');
+  signal patm1 : word_t := (others => '0');
+  signal patm2 : word_t := (others => '0');
   
 begin
 
-  a_clk_diff.p <= a_clk;
-  a_clk_diff.n <= not a_clk;
-  
   o: hwdep.io.io_ddr_output
     port map(
-      p_clk => a_clk_diff,
+      p_clk => a_clk,
       p_d => a_d,
       p_dd => io_a_d
       );
 
   i: hwdep.io.io_ddr_input
     port map(
-      p_clk => b_clk90,
+      p_clk => b_clk,
       p_dd => io_b_d,
       p_d => b_d
       );
 
   cko: hwdep.io.io_ddr_output
     port map(
-      p_clk => a_clk_diff,
+      p_clk => a_clk,
       p_d => "01",
-      p_dd => io_a_clk
+      p_dd => io_clk
       );
 
-  io_b_d <= io_a_d after 300 ps;
-  io_b_clk <= io_a_clk after 300 ps;
+  a_clk.p <= clk;
+  a_clk.n <= not clk;
 
-  b_clk <= io_b_clk after 10 ps;
-  b_clk90.p <= io_b_clk after 0.25 ns;
-  b_clk90.n <= not io_b_clk after 0.25 ns;
+  b_clk.p <= io_clk after 300 ps;
+  b_clk.n <= not b_clk.p;
 
-  clk: process
+  io_b_d <= io_a_d after 350 ps;
+
+  shift: process(clk)
+  begin
+    if rising_edge(clk) then
+      patm2 <= patm1 after 300 ps;
+      patm1 <= pat after 300 ps;
+    end if;
+  end process;
+  
+  run: process
   begin
     for repeat in 0 to 15
     loop
       for i in 0 to 4
       loop
         wait for 1 ns;
-        a_clk <= '0';
+        clk <= '0';
         wait for 1 ns;
-        a_clk <= '1';
+        clk <= '1';
       end loop;
     
       for i in c_pattern1'range
       loop
         wait for 1 ns;
-        a_clk <= '0';
+        pat <= c_pattern1(i) & c_pattern0(i);
+        clk <= '0';
         wait for 0.5 ns;
-        a_d <= c_pattern1(i) & c_pattern0(i);
+        a_d <= pat;
         wait for 0.5 ns;
-        a_clk <= '1';
---        assert s_bin = s_bin2 report "Bad encoding or decoding" severity failure;
+        clk <= '1';
       end loop;
 
       for i in 0 to 4
       loop
         wait for 1 ns;
-        a_clk <= '0';
+        clk <= '0';
         wait for 1 ns;
-        a_clk <= '1';
+        clk <= '1';
       end loop;
     
     end loop;
 
     wait;
+  end process;
+
+  check: process
+  begin
+    wait until rising_edge(clk);
+    assert patm2 = b_d report "Bad encoding or decoding" severity warning;
   end process;
   
 end;
