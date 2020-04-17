03d92b685930320cf88d4da404cdbd92bd380cde
Nicolas Pouillon <nipo@ssji.net>
2020-04-17 19:43:51 +0200

    test: Update gray bench

--- tests/gray/src/tb.vhd
+++ tests/gray/src/tb.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library util;
+library nsl_math;
 
 entity tb is
 end tb;
@@ -10,42 +10,21 @@ end tb;
 architecture arch of tb is
 
   constant data_width : integer := 8;
-  subtype word_t is std_ulogic_vector(data_width-1 downto 0);
-  
-  signal s_bin : word_t := (others => '0');
-  signal s_gray : word_t;
-  signal s_bin2 : word_t := (others => '0');
+  subtype word_t is unsigned(data_width-1 downto 0);
   
 begin
 
-  encoder: util.gray.gray_encoder
-    generic map(
-      data_width => data_width
-      )
-    port map(
-      p_binary => s_bin,
-      p_gray => s_gray
-      );
-
-  decoder: util.gray.gray_decoder
-    generic map(
-      data_width => data_width
-      )
-    port map(
-      p_gray => s_gray,
-      p_binary => s_bin2
-      );
-
   process
+    variable s_bin : word_t := (others => '0');
+    variable s_bin2 : word_t := (others => '0');
   begin
-
     for i in 0 to 2**data_width-1
     loop
-      wait for 1 ns;
-      s_bin <= std_ulogic_vector(unsigned(s_bin) + 1);
+      s_bin2 := nsl_math.gray.gray_to_bin(nsl_math.gray.bin_to_gray(s_bin));
       assert s_bin = s_bin2 report "Bad encoding or decoding" severity failure;
+      s_bin := s_bin + 1;
     end loop;
-
+    assert false report "OK" severity note;
     wait;
   end process;
   
