47ed811a21a03a8e16b4eb86461eaec550c44cc8
Nicolas Pouillon <nipo@ssji.net>
2020-04-30 13:24:00 +0200

    tests: Use logging

--- tests/crc/src/tb.vhd
+++ tests/crc/src/tb.vhd
@@ -8,6 +8,7 @@ use nsl_data.endian.all;
 use nsl_data.crc.all;
 use nsl_simulation.text.all;
 use nsl_simulation.assertions.all;
+use nsl_simulation.logging.all;
 
 entity tb is
 end tb;
@@ -26,60 +27,60 @@ architecture arch of tb is
 begin
 
   iso14443_3: process
-    constant d1234 : byte_string := from_hex("1234");
-    constant d0000 : byte_string := from_hex("0000");
+    constant context: log_context := "ISO-14443-3";
   begin
-    
-    assert_equal("ISO-14443-3 0000",
-                 std_ulogic_vector(crc_iso_14443_3_a_update(crc_iso_14443_3_a_init, d0000)),
+    assert_equal(context, "0000",
+                 std_ulogic_vector(crc_iso_14443_3_a_update(crc_iso_14443_3_a_init, from_hex("0000"))),
                  x"1ea0",
                  failure);
 
-    assert_equal("ISO-14443-3 1234",
-                 std_ulogic_vector(crc_iso_14443_3_a_update(crc_iso_14443_3_a_init, d1234)),
+    assert_equal(context, "1234",
+                 std_ulogic_vector(crc_iso_14443_3_a_update(crc_iso_14443_3_a_init, from_hex("1234"))),
                  x"cf26",
                  failure);
 
-    assert false report "ISO-14443-3 done" severity note;
+    log_info(context, "done");
     wait;
   end process;
 
   ieee_802_3: process
+    constant context: log_context := "IEEE-802.3";
     constant data : byte_string := from_hex( "20cf301acea16238e0c2bd3008060001"
                                             &"0800060400016238e0c2bd300a2a2a01"
                                             &"0000000000000a2a2a02000000000000"
                                             &"00000000000000000000000022b72660");
   begin
     
-    assert_equal("IEEE-802.3 compare",
+    assert_equal(context, "compare",
                  to_le(unsigned(crc_ieee_802_3_update(crc_ieee_802_3_init, data(0 to 59)))),
                  data(60 to 63),
                  failure);
 
-    assert_equal("IEEE-802.3 check constant",
+    assert_equal(context, "check constant",
                  std_ulogic_vector(crc_ieee_802_3_update(crc_ieee_802_3_init, data)),
                  std_ulogic_vector(crc_ieee_802_3_check),
                  failure);
 
-    assert false report "IEEE-802.3 done" severity note;
+    log_info(context, "done");
     wait;
   end process;
 
   other: process
+    constant context: log_context := "Other";
     constant data : byte_string := from_hex("313233343536373839d9c60b34");
   begin
     
-    assert_equal("Other compare",
+    assert_equal(context, "checking CRC to appended",
                  to_le(unsigned(crc_test_update(crc_test_init, data(0 to 8)))),
                  data(9 to 12),
                  failure);
 
-    assert_equal("Other check constant",
+    assert_equal(context, "checking whole message CRC to constant",
                  std_ulogic_vector(crc_test_update(crc_test_init, data)),
                  std_ulogic_vector(crc_test_check),
                  failure);
 
-    assert false report "Other done" severity note;
+    log_info(context, "done");
     wait;
   end process;
   
