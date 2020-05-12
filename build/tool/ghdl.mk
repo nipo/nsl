e8b2061575bf49b5c971d7f39de3e948da77f60a
Nicolas Pouillon <nipo@ssji.net>
2020-05-12 16:53:19 +0200

    ghdl: Update build method

--- build/tool/ghdl.mk
+++ build/tool/ghdl.mk
@@ -11,13 +11,13 @@ _GHDL_STD_93=93c
 _GHDL_STD_08=08
 _GHDL_CF_SUFFIX_93=93
 _GHDL_CF_SUFFIX_08=08
-workdir = $(if $(filter $(top-lib),$1),,ghdl-build/$1/)
-lib_cf = $(call workdir,$1)$1-obj$(_GHDL_CF_SUFFIX_$($1-vhdl-version)).cf
+workdir = $(if $(filter $(top-lib),$1),.,ghdl-build)
+lib_cf = $(call workdir,$1)/$1-obj$(_GHDL_CF_SUFFIX_$($1-vhdl-version)).cf
 
 define ghdl-source-run
 $(GHDL) $1 \
 		--workdir=$(call workdir,$($2-library)) \
-		$(foreach l,$($($2-library)-libdeps-unsorted),-Pghdl-build/$l) \
+		$(sort $(foreach l,$($($2-library)-libdeps-unsorted),-P$(call workdir,$l))) \
 		--std=$(_GHDL_STD_$($($2-library)-vhdl-version)) \
 		-v \
 		--work=$($2-library) \
@@ -33,16 +33,12 @@ endef
 
 define ghdl-library-rules
 
-$(call lib_cf,$1): $(foreach l,$($1-libdeps-unsorted),$(call lib_cf,$($l-library))) $($1-sources) $(MAKEFILE_LIST)
-	$(SILENT)echo Updating $1
-	$(SILENT)echo   packages: $($1-packages)
-	$(SILENT)echo   lib deps: $($1-libdeps-unsorted)
+$(call lib_cf,$1): $(foreach l,$($1-libdeps-unsorted),$(call lib_cf,$l)) $($1-sources) $(MAKEFILE_LIST)
+	# 
 	$(SILENT)mkdir -p $$(dir $$@)
 	$(SILENT)$(foreach s,$($1-sources),$(call $($s-language)-source-do,$s))
 
 clean-dirs += $(if $(filter $1,$(top-lib)),,$(call lib_cf,$1))
-
-$(target): $(call lib_cf,$1)
 
 endef
 
@@ -50,15 +46,15 @@ clean-dirs += ghdl-build
 
 $(eval $(foreach l,$(libraries),$(call ghdl-library-rules,$l)))
 
-$(target): FORCE
+$(target): $(foreach l,$(libraries),$(call lib_cf,$l))
 	$(SILENT)$(GHDL) -e -v \
-		$(GHDL_OPTS) \
+		--std=$(_GHDL_STD_$($(top-lib)-vhdl-version)) \
 		$(sort $(foreach l,$(filter %.cf,$^),-P$(dir $l))) \
 		$(top-entity)
 
 $(target).ghw: $(target)
 	$(SILENT)$(GHDL) -r -v \
-		$(GHDL_OPTS) \
+		--std=$(_GHDL_STD_$($(top-lib)-vhdl-version)) \
 		$(sort $(foreach l,$(filter %.cf,$^),-P$(dir $l))) \
 		$(top-entity) --wave=$@
 
