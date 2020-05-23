7d93a73e9ebc2b286e9616f6629dc31494817a53
Nicolas Pouillon <nipo@ssji.net>
2020-05-23 10:59:14 +0200

    tool/ghdl: Update build

--- build/tool/ghdl.mk
+++ build/tool/ghdl.mk
@@ -11,51 +11,37 @@ _GHDL_STD_93=93c
 _GHDL_STD_08=08
 _GHDL_CF_SUFFIX_93=93
 _GHDL_CF_SUFFIX_08=08
-workdir = $(if $(filter $(top-lib),$1),.,ghdl-build)
+#workdir = $(if $(filter $(top-lib),$1),.,ghdl-build)
+workdir = ghdl-build
 lib_cf = $(call workdir,$1)/$1-obj$(_GHDL_CF_SUFFIX_$($1-vhdl-version)).cf
-
-define ghdl-source-run
-$(GHDL) $1 \
-		--workdir=$(call workdir,$($2-library)) \
-		$(sort $(foreach l,$($($2-library)-libdeps-unsorted),-P$(call workdir,$l))) \
-		--std=$(_GHDL_STD_$($($2-library)-vhdl-version)) \
-		-v \
-		--work=$($2-library) \
-		$2
-	$(SILENT)
-endef
-
-define vhdl-source-do
-$(call ghdl-source-run,-a,$1)
-	$(SILENT)$(call ghdl-source-run,-i,$1)
-	$(SILENT)
-endef
-
-define ghdl-library-rules
-
-$(call lib_cf,$1): $(foreach l,$($1-libdeps-unsorted),$(call lib_cf,$l)) $($1-sources) $(MAKEFILE_LIST)
-	# 
-	$(SILENT)mkdir -p $$(dir $$@)
-	$(SILENT)$(foreach s,$($1-sources),$(call $($s-language)-source-do,$s))
-
-clean-dirs += $(if $(filter $1,$(top-lib)),,$(call lib_cf,$1))
-
-endef
 
 clean-dirs += ghdl-build
 
-$(eval $(foreach l,$(libraries),$(call ghdl-library-rules,$l)))
+define ghdl-library-rules
+	$(SILENT)$(GHDL) -i \
+		--workdir=$(call workdir,$1) \
+		$(sort $(foreach l,$($$1-libdeps-unsorted),-P$(call workdir,$l))) \
+		--std=$(_GHDL_STD_$($1-vhdl-version)) \
+		--work=$1 \
+		$($1-sources)
 
-$(target): $(foreach l,$(libraries),$(call lib_cf,$l))
-	$(SILENT)$(GHDL) -e -v \
+endef
+
+$(target):
+	$(SILENT)mkdir -p ghdl-build
+	$(foreach l,$(libraries),$(call ghdl-library-rules,$l))
+	$(SILENT)$(GHDL) -m -v \
+		--workdir=ghdl-build \
 		--std=$(_GHDL_STD_$($(top-lib)-vhdl-version)) \
-		$(sort $(foreach l,$(filter %.cf,$^),-P$(dir $l))) \
-		$(top-entity)
+		-Pghdl-build \
+		--work=$(top-lib) $(top-entity)
+	$(SILENT)$(GHDL) -c -v \
+		--workdir=ghdl-build \
+		--std=$(_GHDL_STD_$($(top-lib)-vhdl-version)) \
+		-Pghdl-build \
+		--work=$(top-lib) -e $(top-entity)
 
 $(target).ghw: $(target)
-	$(SILENT)$(GHDL) -r -v \
-		--std=$(_GHDL_STD_$($(top-lib)-vhdl-version)) \
-		$(sort $(foreach l,$(filter %.cf,$^),-P$(dir $l))) \
-		$(top-entity) --wave=$@
+	./$< --wave=$@
 
 clean-files += *.o $(top) $(top).ghw $(top).vcd *.cf *.lst $(target)
