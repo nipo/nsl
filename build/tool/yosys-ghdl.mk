45d775f5cd7edf6829f9939c13db8706646f8031
Nicolas Pouillon <nipo@ssji.net>
2020-05-28 16:47:11 +0200

    build: Early support for yosys-based GHDL synthesis

---
+++ build/tool/yosys-ghdl.mk
@@ -0,0 +1,43 @@
+GHDL=ghdl
+GHDL_MODULE=-m /usr/local/lib/ghdl_yosys.so
+YOSYS=yosys
+target ?= $(top-entity)
+GHDL_PRE=
+PNR_PRE=
+ICESTORM_PRE=
+
+-include local.mk
+
+all: $(target).bin
+
+.PRECIOUS: $(target).bin
+
+.PHONY: FORCE
+
+_GHDL_STD_93=93c
+_GHDL_STD_08=08
+_GHDL_CF_SUFFIX_93=93
+_GHDL_CF_SUFFIX_08=08
+
+clean-dirs += yosys-ghdl-build
+
+define ghdl-library-rules
+	$(if $(foreach s,$($1-sources),$(if $(filter $($s-language),vhdl),$s)),$(SILENT)cd $(dir $@) ; $(GHDL) -a --std=$(_GHDL_STD_$($1-vhdl-version)) --work=$1 $(foreach s,$($1-sources),$(if $(filter $($s-language),vhdl),$s)))
+
+endef
+
+yosys-ghdl-build/$(target).json: $(sources) $(MAKEFILE_LIST)
+	mkdir -p $(dir $@)
+	$(foreach l,$(libraries),$(call ghdl-library-rules,$l))
+	$(SILENT)$(GHDL_PRE) bash -c "cd $(PWD)/$(dir $@) ; yosys $(GHDL_MODULE) -p 'ghdl $(top-entity); synth_ice40 -json $(notdir $@)'"
+
+yosys-ghdl-build/$(target).asc: yosys-ghdl-build/$(target).json
+	$(SILENT)$(PNR_PRE) bash -c "cd $(PWD)/$(dir $@) ; nextpnr-ice40 --package up5k $(foreach p,$(filter %.pcf,$(sources)),--pcf $p) --asc $(notdir $@) --json $(notdir $<)"
+
+yosys-ghdl-build/$(target).bin: yosys-ghdl-build/$(target).asc
+	$(SILENT)$(ICESTORM_PRE) bash -c "cd $(PWD)/$(dir $@) ; icepack $(notdir $<) $(notdir $@)"
+
+$(target).bin: yosys-ghdl-build/$(target).bin
+	cp $< $@
+
+clean-files += *.o $(target) $(target).bin *.cf *.lst $(target)
