c970157c0afaa5774e0a525abf75c36e97125bc1
Nicolas Pouillon <nipo@ssji.net>
2020-06-21 14:47:29 +0200

    nsl_clocking/async_input: Allow to debounce for many cycles without having a big shift register

--- lib/nsl_clocking/async/async_input.vhd
+++ lib/nsl_clocking/async/async_input.vhd
@@ -1,5 +1,3 @@
---  Copyright (c) 2016, Vincent Defilippi <vincentdefilippi@gmail.com>
-
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -19,62 +17,58 @@ end async_input;
 
 architecture arch of async_input is
 
-  type state_type is (
-    S_IDLE,
-    S_LOW,
-    S_HIGH,
-    S_RISE,
-    S_FALL
-   );
-  signal state: state_type;
+  type regs_t is
+  record
+    cur, prev : std_ulogic;
+    debouncer : natural range 0 to debounce_count_c-1;
+  end record;
 
-  signal sreg: std_ulogic_vector(debounce_count_c - 1 downto 0);
-  constant SREG_MAX: std_ulogic_vector(debounce_count_c - 1 downto 0) := (others => '1');
-  constant SREG_MIN: std_ulogic_vector(debounce_count_c - 1 downto 0) := (others => '0');
+  signal r, rin : regs_t;
+  signal synced : std_ulogic;
 
 begin
 
-  data_o <=
-    '1' when state = S_RISE or state = S_HIGH else
-    '0' when state = S_FALL or state = S_LOW else
-    data_i;
-
-  rising_o <= '1' when state = S_RISE else '0';
-  falling_o <= '1' when state = S_FALL else '0';
-
-  process (clock_i, reset_n_i)
+  sampler: nsl_clocking.async.async_sampler
+    generic map(
+      cycle_count_c => 2,
+      data_width_c => 1
+      )
+    port map(
+      clock_i => clock_i,
+      data_i(0) => data_i,
+      data_o(0) => synced
+      );
+  
+  regs: process(clock_i, reset_n_i)
   begin
     if reset_n_i = '0' then
-      state <= S_IDLE;
+      r.cur <= '0';
+      r.prev <= '0';
+      r.debouncer <= 0;
     elsif rising_edge(clock_i) then
-      case state is
-        when S_IDLE =>
-          if data_i = '0' then
-            state <= S_LOW;
-          else
-            state <= S_HIGH;
-          end if;
-
-        when S_RISE =>
-          state <= S_HIGH;
-
-        when S_FALL =>
-          state <= S_LOW;
-
-        when S_HIGH =>
-          if sreg = SREG_MIN then
-            state <= S_FALL;
-          end if;
-
-        when S_LOW =>
-          if sreg = SREG_MAX then
-            state <= S_RISE;
-          end if;
-      end case;
-
-      sreg <= sreg(debounce_count_c - 2 downto 0) & data_i;
+      r <= rin;
     end if;
   end process;
 
+  transition: process(r, synced)
+  begin
+    rin <= r;
+
+    if r.cur = synced then
+      rin.debouncer <= debounce_count_c - 1;
+    elsif r.debouncer /= 0 then
+      rin.debouncer <= r.debouncer - 1;
+    else
+      rin.debouncer <= debounce_count_c;
+      rin.cur <= synced;
+    end if;
+
+    rin.prev <= r.cur;
+  end process;
+
+  rising_o <= not r.prev and r.cur;
+  falling_o <= r.prev and not r.cur;
+  data_o <= r.cur;
+  
 end arch;
 
