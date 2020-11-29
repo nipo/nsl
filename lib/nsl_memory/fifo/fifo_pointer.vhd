6f8566482b1456b70ac4f192a1020f3fdbaa1cf7
Nicolas Pouillon <nipo@ssji.net>
2020-11-29 09:55:14 +0100

    nsl_memory: Use pipelined counter in fifo pointer

--- lib/nsl_memory/fifo/fifo_pointer.vhd
+++ lib/nsl_memory/fifo/fifo_pointer.vhd
@@ -31,8 +31,8 @@ entity fifo_pointer is
 end fifo_pointer;
 
 -- "Position" is an aggregate of index (either gray or binary depending on
--- /gray_position_c/) on LSBs, and a "carry" bit (MSB) that toggles once
--- every two wrapping. This allows to compare positions unambiguously for
+-- /gray_position_c/) on LSBs, and a "carry" bit (MSB) that toggles each time
+-- the counter wraps. This allows to compare positions unambiguously for
 -- empty/full conditions.
 
 architecture rtl of fifo_pointer is
@@ -41,53 +41,39 @@ architecture rtl of fifo_pointer is
   constant c_idx_high : ptr_t := to_unsigned(wrap_count_c-1, ptr_width_c);
   constant c_is_pow2 : boolean := c_idx_high = (c_idx_high'range => '1');
 
-  type ctr_t is record
-    wrap_toggle: std_logic;
-    value: ptr_t;
-  end record;
-
   type regs_t is record
-    wcounter: ctr_t;
-    local_position : std_ulogic_vector(ptr_width_c downto 0);
+    wrap: std_ulogic;
     running: boolean;
   end record;
 
-  signal s_can_inc, s_ptr_equal, s_in_same_wrap: boolean;
   signal r, rin: regs_t;
-
-  signal peer_wcounter : ctr_t;
-
-  function next_ctr(cur: ctr_t) return ctr_t is
-    variable ret : ctr_t;
-  begin
-    ret := cur;
-
-    if c_is_pow2 then
-      if cur.value = c_idx_high then
-        ret.wrap_toggle := not cur.wrap_toggle;
-      end if;
-      ret.value := cur.value + 1;
-    else
-      if cur.value = c_idx_high then
-        ret.wrap_toggle := not cur.wrap_toggle;
-        ret.value := (others => '0');
-      else
-        ret.value := cur.value + 1;
-      end if;
-    end if;
-    return ret;
-
-  end function;
+  signal s_can_inc, s_ptr_equal, s_in_same_wrap: boolean;
+  signal s_counter, s_peer_counter : ptr_t;
+  signal s_counter_wrap, s_peer_wrap, s_inc : std_ulogic;
 
 begin
 
+  counter: nsl_clocking.intradomain.intradomain_counter
+    generic map(
+      width_c => ptr_t'length,
+      min_c => "0",
+      max_c => c_idx_high,
+      reset_c => "0"
+      )
+    port map(
+      clock_i => clock_i,
+      reset_n_i => reset_n_i,
+
+      increment_i => s_inc,
+      value_o => s_counter,
+      wrap_o => s_counter_wrap
+      );
+  
   regs: process (clock_i, reset_n_i)
   begin
     if rising_edge(clock_i) then
       if reset_n_i = '0' then
-        r.wcounter.value <= (others => '0');
-        r.wcounter.wrap_toggle <= '0';
-        r.local_position <= (others => '0');
+        r.wrap <= '0';
         r.running <= false;
       else
         r <= rin;
@@ -95,54 +81,28 @@ begin
     end if;
   end process;
 
-  local_ptr_bin: if not gray_position_c
-  generate
-    s_ptr_equal <= r.local_position(ptr_t'range) = peer_position_i(ptr_t'range);
-    s_in_same_wrap <= peer_position_i(ptr_width_c) = r.local_position(ptr_width_c);
-  end generate;
-
-  local_ptr_gray: if gray_position_c
-  generate
-    signal a, b: std_ulogic_vector(ptr_width_c-1 downto 0);
-  begin
-    a(ptr_width_c-2 downto 0) <= r.local_position(ptr_width_c-2 downto 0);
-    a(ptr_width_c-1) <= r.local_position(ptr_width_c-1) xor r.local_position(ptr_width_c);
-    b(ptr_width_c-2 downto 0) <= peer_position_i(ptr_width_c-2 downto 0);
-    b(ptr_width_c-1) <= peer_position_i(ptr_width_c-1) xor peer_position_i(ptr_width_c);
-
-    s_ptr_equal <= a = b;
-    s_in_same_wrap <= peer_position_i(ptr_width_c) = r.local_position(ptr_width_c);
-  end generate;
-
-  s_can_inc <= not s_ptr_equal or (s_in_same_wrap = equal_can_move_c);
-
+  s_ptr_equal <= s_counter = s_peer_counter;
+  s_in_same_wrap <= r.wrap = s_peer_wrap;
+  s_can_inc <= r.running and (not s_ptr_equal or (s_in_same_wrap = equal_can_move_c));
   ack_o <= '1' when r.running and s_can_inc else '0';
-  local_position_o <= r.local_position;
-  mem_ptr_o <= r.wcounter.value;
+  mem_ptr_o <= s_counter;
+  s_inc <= inc_i when s_can_inc and r.running else '0';
 
-  transition: process(r, inc_i, s_can_inc)
-    variable next_value : ctr_t;
-    variable local_ptr: std_ulogic_vector(ptr_width_c downto 0);
+  transition: process(r, s_counter_wrap, s_inc)
   begin
     rin <= r;
+
     rin.running <= true;
 
-    if r.running and s_can_inc and inc_i = '1' then
-      next_value := next_ctr(r.wcounter);
-      local_ptr := next_value.wrap_toggle & std_ulogic_vector(next_value.value);
-
-      rin.wcounter <= next_value;
-      if gray_position_c then
-        rin.local_position <= nsl_math.gray.bin_to_gray(unsigned(local_ptr));
-      else
-        rin.local_position <= local_ptr;
-      end if;
+    if s_inc = '1' and s_counter_wrap = '1' then
+      rin.wrap <= not r.wrap;
     end if;
   end process;
 
-  decode_position: if gray_position_c
+  gray_position: if gray_position_c
   generate
     signal peer_ptr_bin_relaxed : unsigned(ptr_width_c downto 0);
+    signal local_ptr : unsigned(ptr_width_c downto 0);
   begin
     decoder: nsl_math.gray.gray_decoder_pipelined
       generic map(
@@ -155,57 +115,66 @@ begin
         binary_o => peer_ptr_bin_relaxed
         );
 
-    peer_wcounter.wrap_toggle <= peer_ptr_bin_relaxed(peer_ptr_bin_relaxed'left);
-    peer_wcounter.value <= peer_ptr_bin_relaxed(peer_ptr_bin_relaxed'left-1 downto 0);
+    s_peer_wrap <= peer_ptr_bin_relaxed(peer_ptr_bin_relaxed'left);
+    s_peer_counter <= peer_ptr_bin_relaxed(peer_ptr_bin_relaxed'left-1 downto 0);
+
+    local_ptr(ptr_t'length) <= r.wrap;
+    local_ptr(ptr_t'range) <= s_counter;
+
+    gray_enc: process(clock_i)
+    begin
+      if rising_edge(clock_i) then
+        local_position_o <= nsl_math.gray.bin_to_gray(local_ptr);
+      end if;
+    end process;
   end generate;
 
-  forward_position: if not gray_position_c
+  bin_position: if not gray_position_c
   generate
-    peer_wcounter.wrap_toggle <= peer_position_i(ptr_t'length);
-    peer_wcounter.value <= unsigned(peer_position_i(ptr_t'range));
+    s_peer_wrap <= peer_position_i(ptr_t'length);
+    s_peer_counter <= unsigned(peer_position_i(ptr_t'range));
+
+    local_position_o(ptr_t'length) <= r.wrap;
+    local_position_o(ptr_t'range) <= std_ulogic_vector(s_counter);
   end generate;
 
-  -- peer_wcounter holds same data as r.wcounter:
-  -- - a toggle of wraps
-  -- - an index
-
-  calc: process(r.wcounter, peer_wcounter)
-    variable head, tail, wrap, used, free : unsigned(ptr_width_c downto 0);
+  free_calc: process(s_counter, s_peer_counter, s_in_same_wrap)
+    variable head, tail, wrap_addedum, used, free : unsigned(ptr_width_c downto 0);
   begin
-    wrap := to_unsigned(wrap_count_c, ptr_width_c + 1);
+    wrap_addedum := to_unsigned(wrap_count_c, ptr_width_c + 1);
 
     -- When wrapping counter is the same, pointer difference is trivial
     -- When in different wrapping counts, we need to add up on complete cycle.
     -- Of course, this is easier for power-of-two wrap counts.
-    if r.wcounter.wrap_toggle = peer_wcounter.wrap_toggle then
+    if s_in_same_wrap then
       if peer_ahead_c then
-        head := ("0" & r.wcounter.value);
-        tail := ("0" & peer_wcounter.value);
+        head := ("0" & s_counter);
+        tail := ("0" & s_peer_counter);
       else
-        head := ("0" & peer_wcounter.value);
-        tail := ("0" & r.wcounter.value);
+        head := ("0" & s_peer_counter);
+        tail := ("0" & s_counter);
       end if;
     else
       if peer_ahead_c then
         -- Unsure whether optimizer sees this one, just in case, do it manually.
         if c_is_pow2 then
-          head := ("1" & r.wcounter.value);
+          head := ("1" & s_counter);
         else
-          head := ("0" & r.wcounter.value) + wrap;
+          head := ("0" & s_counter) + wrap_addedum;
         end if;
-        tail := ("0" & peer_wcounter.value);
+        tail := ("0" & s_peer_counter);
       else
         if c_is_pow2 then
-          head := ("1" & peer_wcounter.value);
+          head := ("1" & s_peer_counter);
         else
-          head := ("0" & peer_wcounter.value) + wrap;
+          head := ("0" & s_peer_counter) + wrap_addedum;
         end if;
-        tail := ("0" & r.wcounter.value);
+        tail := ("0" & s_counter);
       end if;
     end if;
 
     used := head - tail;
-    free := wrap + tail - head;
+    free := wrap_addedum + tail - head;
 
     -- Simulation hack:
     --
