0664c70e888ee0c60c96dd2f76e7890c0a202e03
Nicolas Pouillon <nipo@ssji.net>
2020-12-20 17:18:49 +0100

    nsl_ftdi/ft245: Add global clock buffer and fifo slices

--- lib/nsl_ftdi/ft245/ft245_sync_fifo_master.vhd
+++ lib/nsl_ftdi/ft245/ft245_sync_fifo_master.vhd
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-library nsl_ftdi;
+library nsl_ftdi, nsl_memory, nsl_hwdep, nsl_clocking;
 
 entity ft245_sync_fifo_master is
   generic (
@@ -41,18 +41,40 @@ architecture arch of ft245_sync_fifo_master is
 
   signal r, rin: regs_t;
 
+  signal int_in_ready_i : std_ulogic;
+  signal int_in_valid_o : std_ulogic;
+  signal int_in_data_o  : std_ulogic_vector(7 downto 0);
+  signal int_out_ready_o : std_ulogic;
+  signal int_out_valid_i : std_ulogic;
+  signal int_out_data_i  : std_ulogic_vector(7 downto 0);
+  signal clock : std_ulogic;
+  signal reset_sync : std_ulogic;
+
 begin
 
-  regs: process (bus_i, reset_n_i)
+  clock_buffer: nsl_hwdep.clock.clock_buffer
+    port map(
+      clock_i => bus_i.clk,
+      clock_o => clock
+      );
+
+  reset_ryncer: nsl_clocking.async.async_edge
+    port map(
+      clock_i => clock,
+      data_i => reset_n_i,
+      data_o => reset_sync
+      );
+  
+  regs: process (clock, reset_sync)
   begin
-    if reset_n_i = '0' then
+    if reset_sync = '0' then
       r.state <= ST_RESET;
-    elsif rising_edge(bus_i.clk) then
+    elsif rising_edge(clock) then
       r <= rin;
     end if;
   end process;
   
-  transition: process (r, out_valid_i, in_ready_i, bus_i)
+  transition: process (r, int_out_valid_i, int_in_ready_i, bus_i)
   begin
     rin <= r;
     
@@ -61,28 +83,28 @@ begin
         rin.state <= ST_INBOUND_PRE;
         
       when ST_INBOUND =>
-        if in_ready_i = '1' and bus_i.rxf = '1' then
+        if int_in_ready_i = '1' and bus_i.rxf = '1' then
           if r.counter = 0 then
-            if out_valid_i = '1' and bus_i.txe = '1' then
+            if int_out_valid_i = '1' and bus_i.txe = '1' then
               rin.state <= ST_OUTBOUND_PRE;
             end if;
           else
             rin.counter <= r.counter - 1;
           end if;
-        elsif out_valid_i = '1' and bus_i.txe = '1' then
+        elsif int_out_valid_i = '1' and bus_i.txe = '1' then
           rin.state <= ST_OUTBOUND_PRE;
         end if;
         
       when ST_OUTBOUND =>
-        if out_valid_i = '1' and bus_i.txe = '1' then
+        if int_out_valid_i = '1' and bus_i.txe = '1' then
           if r.counter = 0 then
-            if in_ready_i = '1' and bus_i.rxf = '1' then
+            if int_in_ready_i = '1' and bus_i.rxf = '1' then
               rin.state <= ST_INBOUND_PRE;
             end if;
           else
             rin.counter <= r.counter - 1;
           end if;
-        elsif in_ready_i = '1' and bus_i.rxf = '1' then
+        elsif int_in_ready_i = '1' and bus_i.rxf = '1' then
           rin.state <= ST_INBOUND_PRE;
         end if;
         
@@ -96,25 +118,25 @@ begin
     end case;
   end process;
 
-  clock_o <= bus_i.clk;
-  in_data_o <= bus_i.data;
-  bus_o.data <= out_data_i;
+  clock_o <= clock;
+  int_in_data_o <= bus_i.data;
+  bus_o.data <= int_out_data_i;
 
-  handshaking: process(r, in_ready_i, out_valid_i, bus_i)
+  handshaking: process(r, int_in_ready_i, int_out_valid_i, bus_i)
   begin
-    out_ready_o <= '0';
-    in_valid_o <= '0';
+    int_out_ready_o <= '0';
+    int_in_valid_o <= '0';
     bus_o.rd <= '0';
     bus_o.wr <= '0';
 
     case r.state is
       when ST_INBOUND =>
-        bus_o.rd <= in_ready_i and bus_i.rxf;
-        in_valid_o <= bus_i.rxf;
+        bus_o.rd <= int_in_ready_i and bus_i.rxf;
+        int_in_valid_o <= bus_i.rxf;
 
       when ST_OUTBOUND =>
-        out_ready_o <= bus_i.txe;
-        bus_o.wr <= out_valid_i and bus_i.txe;
+        int_out_ready_o <= bus_i.txe;
+        bus_o.wr <= int_out_valid_i and bus_i.txe;
 
       when others =>
         null;
@@ -140,4 +162,38 @@ begin
     end case;
   end process;
   
+  slice_out: nsl_memory.fifo.fifo_register_slice
+    generic map(
+      data_width_c => 8
+      )
+    port map(
+      reset_n_i => reset_sync,
+      clock_i => clock,
+
+      in_data_i => out_data_i,
+      in_valid_i => out_valid_i,
+      in_ready_o => out_ready_o,
+
+      out_data_o => int_out_data_i,
+      out_valid_o => int_out_valid_i,
+      out_ready_i => int_out_ready_o
+      );
+
+  slice_in: nsl_memory.fifo.fifo_register_slice
+    generic map(
+      data_width_c => 8
+      )
+    port map(
+      reset_n_i => reset_sync,
+      clock_i => clock,
+
+      in_data_i => int_in_data_o,
+      in_valid_i => int_in_valid_o,
+      in_ready_o => int_in_ready_i,
+
+      out_data_o => in_data_o,
+      out_valid_o => in_valid_o,
+      out_ready_i => in_ready_i
+      );
+
 end arch;
