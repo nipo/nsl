56ac395b3cd4fc3202c2b3b2df41077b8f2de55a
Nicolas Pouillon <nipo@ssji.net>
2020-12-27 15:20:46 +0100

    tools: Fix ghdl build

--- build/tool/ghdl.mk
+++ build/tool/ghdl.mk
@@ -11,13 +11,16 @@ _GHDL_STD_93=93c
 _GHDL_STD_08=08
 _GHDL_CF_SUFFIX_93=93
 _GHDL_CF_SUFFIX_08=08
-#workdir = $(if $(filter $(top-lib),$1),.,ghdl-build)
-workdir = ghdl-build
+#workdir = $(if $(filter $(top-lib),$1),ghdl-build/,ghdl-build/$1)
+#workdir = ghdl-build/
+workdir = ghdl-build/$1
 lib_cf = $(call workdir,$1)/$1-obj$(_GHDL_CF_SUFFIX_$($1-vhdl-version)).cf
 
 clean-dirs += ghdl-build
 
 define ghdl-library-rules
+	echo Compiling $(l)
+	$(SILENT)mkdir -p $(call workdir,$1)
 	$(SILENT)$(GHDL) -i \
 		--workdir=$(call workdir,$1) \
 		$(sort $(foreach l,$($$1-libdeps-unsorted),-P$(call workdir,$l))) \
@@ -27,18 +30,18 @@ define ghdl-library-rules
 
 endef
 
-$(target):
+$(target): $(sources) $(MAKEFILE_LIST)
 	$(SILENT)mkdir -p ghdl-build
 	$(foreach l,$(libraries),$(call ghdl-library-rules,$l))
 	$(SILENT)$(GHDL) -m -v \
-		--workdir=ghdl-build \
+		--workdir=$(call workdir,$(top-lib)) \
 		--std=$(_GHDL_STD_$($(top-lib)-vhdl-version)) \
-		-Pghdl-build \
+		$(foreach l,$(libraries),-P$(call workdir,$l)) \
 		--work=$(top-lib) $(top-entity)
 	$(SILENT)$(GHDL) -c -v \
-		--workdir=ghdl-build \
+		--workdir=$(call workdir,$(top-lib)) \
 		--std=$(_GHDL_STD_$($(top-lib)-vhdl-version)) \
-		-Pghdl-build \
+		$(foreach l,$(libraries),-P$(call workdir,$l)) \
 		--work=$(top-lib) -e $(top-entity)
 
 $(target).ghw: $(target)
