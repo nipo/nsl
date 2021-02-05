c20611cc771adcca1c9a5817f1048f728a73bda0
Nicolas Pouillon <nipo@ssji.net>
2021-02-05 12:08:54 +0100

    tool/ghdl: Split support for GHDL with/without LLVM

--- build/tool/ghdl.mk
+++ build/tool/ghdl.mk
@@ -7,23 +7,26 @@ simulate: $(target).ghw
 
 .PHONY: FORCE
 
+GHDL_LLVM :=
+
 _GHDL_STD_93=93c
 _GHDL_STD_08=08
 _GHDL_CF_SUFFIX_93=93
 _GHDL_CF_SUFFIX_08=08
-#workdir = $(if $(filter $(top-lib),$1),ghdl-build/,ghdl-build/$1)
-#workdir = ghdl-build/
-workdir = ghdl-build/$1
+
 lib_cf = $(call workdir,$1)/$1-obj$(_GHDL_CF_SUFFIX_$($1-vhdl-version)).cf
 
 clean-dirs += ghdl-build
 
-define ghdl-library-rules
-	echo Compiling $(l)
-	$(SILENT)mkdir -p $(call workdir,$1)
-	$(SILENT)$(GHDL) -i \
+ifeq ($(GHDL_LLVM),)
+# GHDL Without LLVM
+
+workdir = $(if $(filter $(top-lib),$1),.,ghdl-build)
+
+define ghdl-library-analyze-rules
+	$(SILENT)$(GHDL) -a \
 		--workdir=$(call workdir,$1) \
-		$(sort $(foreach l,$($$1-libdeps-unsorted),-P$(call workdir,$l))) \
+		$(sort $(foreach l,$($1-libdeps-unsorted),-P$(call workdir,$l))) \
 		--std=$(_GHDL_STD_$($1-vhdl-version)) \
 		$($l-ghdl-flags) \
 		--work=$1 \
@@ -31,9 +34,34 @@ define ghdl-library-rules
 
 endef
 
-$(target): $(sources) $(MAKEFILE_LIST)
-	$(SILENT)mkdir -p ghdl-build
-	$(foreach l,$(libraries),$(call ghdl-library-rules,$l))
+define ghdl-compile-rules
+	$(SILENT)$(GHDL) -m \
+		--workdir=$(call workdir,$(top-lib)) \
+		$(foreach l,$(libraries),-P$(call workdir,$l)) \
+		$(sort $(foreach l,$(libraries),-P$(call workdir,$(top-lib)))) \
+		--work=$(top-lib) $(top-entity)
+	$(SILENT)$(GHDL) -e \
+		--workdir=$(call workdir,$(top-lib)) \
+		$(foreach l,$(libraries),-P$(call workdir,$l)) \
+		$(sort $(foreach l,$(libraries),-P$(call workdir,$(top-lib)))) \
+		--work=$(top-lib) $(top-entity)
+endef
+
+define ghdl-run-rules
+	$(SILENT)$(GHDL) -r \
+		--workdir=$(call workdir,$(top-lib)) \
+		$(foreach l,$(libraries),-P$(call workdir,$l)) \
+		$(sort $(foreach l,$(libraries),-P$(call workdir,$(top-lib)))) \
+		--work=$(top-lib) $(top-entity) \
+		 --wave=$@ --ieee-asserts=disable
+endef
+
+else
+# GHDL + LLVM mode
+
+workdir = ghdl-build/$1
+ghdl-library-analyze-rules :=
+define ghdl-compile-rules
 	$(SILENT)$(GHDL) -m -v \
 		--workdir=$(call workdir,$(top-lib)) \
 		--std=$(_GHDL_STD_$($(top-lib)-vhdl-version)) \
@@ -47,7 +75,34 @@ $(target): $(sources) $(MAKEFILE_LIST)
 		$(foreach l,$(libraries),$($l-ghdl-flags)) \
 		--work=$(top-lib) -e $(top-entity)
 
-$(target).ghw: $(target)
+endef
+
+define ghdl-run-rules
 	./$< --wave=$@
+endef
+
+endif
+
+define ghdl-library-rules
+	echo Compiling $(l)
+	$(SILENT)mkdir -p $(call workdir,$1)
+	$(SILENT)$(GHDL) -i \
+		--workdir=$(call workdir,$1) \
+		$(sort $(foreach l,$($1-libdeps-unsorted),-P$(call workdir,$l))) \
+		--std=$(_GHDL_STD_$($1-vhdl-version)) \
+		$($l-ghdl-flags) \
+		--work=$1 \
+		$(foreach f,$($1-sources),$(if $(filter vhdl,$($f-language)),$f))
+$(call ghdl-library-analyze-rules,$1)
+
+endef
+
+$(target): $(sources) $(MAKEFILE_LIST)
+	$(SILENT)mkdir -p ghdl-build
+	$(foreach l,$(libraries),$(if $(foreach f,$($l-sources),$(if $(filter vhdl,$($f-language)),$f)),$(call ghdl-library-rules,$l)))
+	$(call ghdl-compile-rules)
+
+$(target).ghw: $(target)
+	$(call ghdl-run-rules)
 
 clean-files += *.o $(top) $(top).ghw $(top).vcd *.cf *.lst $(target)
