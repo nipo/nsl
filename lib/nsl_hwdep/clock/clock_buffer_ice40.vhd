fd8ae4b9c6efe643a51b09dc552ebb264812436b
Nicolas Pouillon <nipo@ssji.net>
2021-02-15 18:02:43 +0100

    nsl_hwdep/clock: Actually implement clock buffer for iCE40

--- lib/nsl_hwdep/clock/clock_buffer_ice40.vhd
+++ lib/nsl_hwdep/clock/clock_buffer_ice40.vhd
@@ -10,8 +10,19 @@ end entity;
 
 architecture ice of clock_buffer is
 
+  component SB_GB
+    port (
+      user_signal_to_global_buffer:in std_logic;
+      global_buffer_output:out std_logic
+      );
+  end component;
+
 begin
 
-  clock_o <= clock_i;
+  gb: sb_gb
+    port map(
+      user_signal_to_global_buffer => clock_i,
+      global_buffer_output => clock_o
+      );
   
 end architecture;
