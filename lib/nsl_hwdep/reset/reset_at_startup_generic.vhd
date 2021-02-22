cbb18cf6cf41b474835fc5a2d751233ff82f9f7e
Nicolas Pouillon <nipo@ssji.net>
2021-02-22 11:30:58 +0100

    nsl_hwdep/reset: Try to thwart optimizer in reset_at_startup_generic

--- lib/nsl_hwdep/reset/reset_at_startup_generic.vhd
+++ lib/nsl_hwdep/reset/reset_at_startup_generic.vhd
@@ -10,18 +10,21 @@ end entity;
 
 architecture gen of reset_at_startup is
 
-  signal sh : std_ulogic_vector(0 to 8) := (others => '0');
+  signal sh : std_ulogic_vector(0 to 7) := (others => '0');
   attribute keep : string;
   attribute keep of sh : signal is "TRUE";
 
 begin
 
-  reset_n_o <= sh(0);
-
   gen: process(clock_i)
   begin
     if rising_edge(clock_i) then
-      sh <= sh(1 to 8) & '1';
+      sh <= sh(1 to 7) & not sh(7);
+      if sh = "01010101" or sh = "10101010" then
+        reset_n_o <= '1';
+      else
+        reset_n_o <= '0';
+      end if;
     end if;
   end process;
 
