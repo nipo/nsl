69279493a7774287d6ec2013fdc6d7f5b5369138
Nicolas Pouillon <nipo@ssji.net>
2021-02-26 09:03:08 +0100

    nsl_data/endian: Add byteswap/bitswap primitives

--- lib/nsl_data/endian/endian.pkg.vhd
+++ lib/nsl_data/endian/endian.pkg.vhd
@@ -10,10 +10,34 @@ package endian is
   function from_le(data : byte_string) return unsigned;
   function to_be(word : unsigned) return byte_string;
   function from_be(data : byte_string) return unsigned;
+  function bitswap(x: std_ulogic_vector) return std_ulogic_vector;
+  function byteswap(x: std_ulogic_vector) return std_ulogic_vector;
+  function byteswap(x: unsigned) return unsigned;
 
 end package endian;
 
 package body endian is
+
+  function bitswap(x:std_ulogic_vector) return std_ulogic_vector is
+    alias xx: std_ulogic_vector(0 to x'length - 1) is x;
+    variable rx: std_ulogic_vector(x'length - 1 downto 0);
+  begin
+    for i in xx'range
+    loop
+      rx(i) := xx(i);
+    end loop;
+    return rx;
+  end function;
+
+  function byteswap(x: unsigned) return unsigned is
+  begin
+    return from_le(to_be(x));
+  end function;
+
+  function byteswap(x:std_ulogic_vector) return std_ulogic_vector is
+  begin
+    return std_ulogic_vector(byteswap(unsigned(x)));
+  end function;
 
   function to_le(word : unsigned) return byte_string is
     variable mem_data : byte_string(0 to word'length/8-1);
