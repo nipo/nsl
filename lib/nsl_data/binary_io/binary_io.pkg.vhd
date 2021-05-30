64af4e1ad42ceb0adb2b0e2cac577f11aba254e7
Nicolas Pouillon <nipo@ssji.net>
2021-05-30 21:32:20 +0200

    nsl_data: Add binary IO package

---
+++ lib/nsl_data/binary_io/binary_io.pkg.vhd
@@ -0,0 +1,41 @@
+library ieee;
+use std.textio.all;
+
+library nsl_data;
+use nsl_data.bytestream.all;
+
+package binary_io is
+
+  alias binary_file_item is character;
+  type binary_file is file of binary_file_item;
+
+  procedure read(file handle: binary_file; data: out byte_string);
+  procedure write(file handle: binary_file; data: in byte_string);
+
+end package;
+
+package body binary_io is
+
+  procedure read(file handle: binary_file; data: out byte_string)
+  is
+    variable tmp: binary_file_item;
+  begin
+    for i in data'range
+    loop
+      read(handle, tmp);
+      data(i) := to_byte(tmp);
+    end loop;
+  end procedure;
+
+  procedure write(file handle: binary_file; data: in byte_string)
+  is
+    variable tmp: binary_file_item;
+  begin
+    for i in data'range
+    loop
+      tmp := to_character(data(i));
+      write(handle, tmp);
+    end loop;
+  end procedure;
+
+end package body;
