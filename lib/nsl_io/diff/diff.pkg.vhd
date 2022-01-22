c152e19b34bd405d943bac7a50c668415fd4daff
Nicolas Pouillon <nipo@ssji.net>
2022-01-22 14:49:28 +0100

    nsl_io/diff: Add single-ended from/to converters

--- lib/nsl_io/diff/diff.pkg.vhd
+++ lib/nsl_io/diff/diff.pkg.vhd
@@ -10,4 +10,26 @@ package diff is
 
   type diff_pair_vector is array(natural range <>) of diff_pair;
 
+  function to_diff(v: std_ulogic) return diff_pair;
+  function to_se(d: diff_pair) return std_ulogic;
+
 end package diff;
+
+package body diff is
+
+  function to_diff(v: std_ulogic) return diff_pair
+  is
+    variable ret : diff_pair;
+  begin
+    ret.p := v;
+    ret.n := not v;
+    return ret;
+  end function;
+
+  function to_se(d: diff_pair) return std_ulogic
+  is
+  begin
+    return d.p;
+  end function;
+
+end package body diff;
