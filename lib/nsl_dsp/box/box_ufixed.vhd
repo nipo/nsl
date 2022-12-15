f47a169284ad78e4fe5fd0928cbb81e29669f322
Nicolas Pouillon <nipo@ssji.net>
2022-12-15 18:25:23 +0100

    nsl_dsp: Use delay_line to implement box filter

--- lib/nsl_dsp/box/box_ufixed.vhd
+++ lib/nsl_dsp/box/box_ufixed.vhd
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-library nsl_math;
+library nsl_math, work;
 use nsl_math.fixed.all;
 
 entity box_ufixed is
@@ -12,6 +12,7 @@ entity box_ufixed is
     clock_i: in std_ulogic;
     reset_n_i : in std_ulogic;
 
+    valid_i : in std_ulogic := '1';
     in_i : in ufixed;
     out_o : out ufixed
     );
@@ -19,22 +20,23 @@ end entity;
 
 architecture beh of box_ufixed is
 
-  subtype sample_t is ufixed(in_i'left downto in_i'right);
-  type sample_vector is array (integer range <>) of sample_t;
   subtype acc_t is ufixed(in_i'left + count_l2_c downto in_i'right);
+  subtype dacc_t is ufixed(in_i'left downto in_i'right - count_l2_c);
 
   type regs_t is
   record
-    delay_line : sample_vector(0 to 2**count_l2_c-1);
     acc : acc_t;
   end record;
 
   signal r, rin : regs_t;
+  signal ready_s: std_ulogic;
+  signal oacc_s : dacc_t;
+  signal in_i_s, delayed_s: std_ulogic_vector(in_i'length-1 downto 0);
 
 begin
 
-  assert in_i'left = out_o'left and in_i'right = out_o'right
-    report "Input and output data words are not the same size"
+  assert in_i'left = out_o'left and in_i'right >= out_o'right
+    report "Input/output should have same LSB, output may have more bits"
     severity failure;
 
   reg: process(clock_i, reset_n_i) is
@@ -44,19 +46,42 @@ begin
     end if;
 
     if reset_n_i = '0' then
-      r.delay_line <= (others => (others => '0'));
       r.acc <= (others => '0');
     end if;
   end process;
 
-  transition: process(r, in_i) is
+  delay_line: work.delay_line.delay_line_memory
+    generic map(
+      data_width_c => in_i'length,
+      cycles_c => 2 ** count_l2_c
+      )
+    port map(
+      clock_i => clock_i,
+      reset_n_i => reset_n_i,
+
+      ready_o => ready_s,
+      valid_i => valid_i,
+      data_i => in_i_s,
+      data_o => delayed_s
+      );
+
+  in_i_s <= to_suv(in_i);
+  
+  transition: process(r, in_i, delayed_s, valid_i, ready_s) is
+    variable delayed: ufixed(in_i'range);
   begin
-    rin.delay_line <= r.delay_line(1 to r.delay_line'right) & in_i;
-    rin.acc <= r.acc
-               - resize(r.delay_line(0), acc_t'left, acc_t'right)
-               + resize(in_i, acc_t'left, acc_t'right);
+    rin <= r;
+
+    delayed := ufixed(delayed_s);
+
+    if valid_i = '1' and ready_s = '1' then
+      rin.acc <= r.acc
+                 - resize(delayed, acc_t'left, acc_t'right)
+                 + resize(in_i, acc_t'left, acc_t'right);
+    end if;
   end process;
 
-  out_o <= resize(shr(r.acc, count_l2_c), out_o'left, out_o'right);
+  oacc_s <= r.acc;
+  out_o <= resize(oacc_s, out_o'left, out_o'right);
 
 end architecture;
