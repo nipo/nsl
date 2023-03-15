d43279afdc1b69bdfb45aebfaf62f0fa26befd27
Nicolas Pouillon <nipo@ssji.net>
2023-03-15 14:00:26 +0100

    nsl_i2c/mcp4726: Add voltage reference support in initer

--- lib/nsl_i2c/mcp4726/mcp4726.pkg.vhd
+++ lib/nsl_i2c/mcp4726/mcp4726.pkg.vhd
@@ -12,7 +12,23 @@ package mcp4726 is
 
   function mcp4726_addr(sel: integer range 0 to 7) return unsigned;
 
+  type vref_t is (
+    VREF_VDD,
+    VREF_UNBUFFERED,
+    VREF_BUFFERED
+    );
+
+  type power_t is (
+    POWER_ON,
+    POWER_OFF_1k,
+    POWER_OFF_100k,
+    POWER_OFF_500k
+    );
+  
   function mcp4726_init(saddr: unsigned;
+                        vref: vref_t;
+                        power: power_t;
+                        gain: integer range 1 to 2;
                         value: unsigned(11 downto 0)) return byte_string;
 
   -- MCP4726 writer
@@ -52,11 +68,40 @@ package body mcp4726 is
   end function;
 
   function mcp4726_init(saddr: unsigned;
+                        vref: vref_t;
+                        power: power_t;
+                        gain: integer range 1 to 2;
                         value: unsigned(11 downto 0)) return byte_string
   is
+    variable vref_u, pd_u : unsigned(1 downto 0);
+    variable g_u: unsigned(0 downto 0);
   begin
+    assert vref /= VREF_VDD or gain /= 2
+      report "VREF = VDD and GAIN = 2 is not possible"
+      severity failure;
+
+    case vref is
+      when VREF_VDD => vref_u := "00";
+      when VREF_UNBUFFERED => vref_u := "10";
+      when others => vref_u := "11";
+    end case;
+
+    case power is
+      when POWER_ON => pd_u := "00";
+      when POWER_OFF_1k => pd_u := "01";
+      when POWER_OFF_100k => pd_u := "10";
+      when others => pd_u := "11";
+    end case;
+
+    if gain = 1 then
+      g_u := "0";
+    else
+      g_u := "1";
+    end if;
+    
     return null_byte_string
-      & i2c_write(saddr, to_be("0000" & value))
+      & i2c_write(saddr, to_be("010" & vref_u & pd_u & g_u
+                               & "0000" & value))
       ;
   end function;
 
