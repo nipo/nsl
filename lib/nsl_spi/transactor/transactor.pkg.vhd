7a6c37b62a869ff08c1c81e55e14b432574b8c0c
Nicolas Pouillon <nipo@ssji.net>
2023-06-01 15:20:40 +0200

    nsl_spi/transactor: Add functions to spill transactions

--- lib/nsl_spi/transactor/transactor.pkg.vhd
+++ lib/nsl_spi/transactor/transactor.pkg.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library nsl_bnoc, nsl_spi, nsl_io;
+library nsl_bnoc, nsl_spi, nsl_io, nsl_data, nsl_logic;
 
 package transactor is
 
@@ -21,6 +21,27 @@ package transactor is
   constant SPI_CMD_UNSELECT     : nsl_bnoc.framed.framed_data_t := "000--111";
   constant SPI_CMD_DIV          : nsl_bnoc.framed.framed_data_t := "001-----";
 
+  function spi_select(cs: integer := -1;
+                      cpol : std_ulogic := '0';
+                      cpha : std_ulogic := '0')
+    return nsl_data.bytestream.byte_string;
+
+  function spi_select(cs: integer := -1;
+                      mode : integer range 0 to 3 := 0)
+    return nsl_data.bytestream.byte_string;
+
+  function spi_div(divisor : integer range 1 to 32)
+    return nsl_data.bytestream.byte_string;
+
+  function spi_clock(clock_hz: real; sck_hz: real)
+    return nsl_data.bytestream.byte_string;
+
+  function spi_shift(byte_count: integer range 1 to 64; read_miso : boolean)
+    return nsl_data.bytestream.byte_string;
+
+  function spi_shift(mosi: nsl_data.bytestream.byte_string; read_miso : boolean)
+    return nsl_data.bytestream.byte_string;
+  
   component spi_framed_transactor
     generic(
       slave_count_c : natural range 1 to 7 := 1
@@ -64,3 +85,88 @@ package transactor is
   end component;
 
 end package transactor;
+
+package body transactor is
+
+  use nsl_logic.bool.all;
+  
+  function spi_select(cs: integer := -1;
+                      cpol : std_ulogic := '0';
+                      cpha : std_ulogic := '0')
+    return nsl_data.bytestream.byte_string
+  is
+    variable ret: nsl_data.bytestream.byte_string(0 to 0);
+  begin
+    ret(0) := "000" & cpol & cpha & std_ulogic_vector(to_unsigned(cs mod 8, 3));
+
+    return ret;
+  end function;
+
+  function spi_select(cs: integer := -1;
+                      mode : integer range 0 to 3 := 0)
+    return nsl_data.bytestream.byte_string
+  is
+    constant mode_v : unsigned(1 downto 0) := to_unsigned(mode, 2);
+  begin
+    return spi_select(cs, mode_v(1), mode_v(0));
+  end function;
+
+  function spi_div(divisor : integer range 1 to 32)
+    return nsl_data.bytestream.byte_string
+  is
+    variable ret: nsl_data.bytestream.byte_string(0 to 0);
+  begin
+    ret(0) := "001" & std_ulogic_vector(to_unsigned(divisor-1, 5));
+
+    return ret;
+  end function;
+
+  function spi_clock(clock_hz: real; sck_hz: real)
+    return nsl_data.bytestream.byte_string
+  is
+    constant ratio : real := clock_hz / sck_hz / 2.0;
+  begin
+    if ratio < 1.0 then
+      return spi_div(1);
+    elsif ratio > 31.0 then
+      return spi_div(32);
+    else
+      return spi_div(integer(ceil(ratio)));
+    end function;
+  end function;
+
+  function spi_shift(byte_count: integer range 1 to 64; read_miso : boolean)
+    return nsl_data.bytestream.byte_string
+  is
+    variable ret: nsl_data.bytestream.byte_string(0 to 0);
+    constant pad: nsl_data.bytestream.byte_string(1 to byte_count) := (others => "00000000");
+  begin
+    -- We have no option to have no input/no output shift, so shift zeros
+    -- when we have no miso reading.
+    if read_miso then
+      ret(0) := "01" & std_ulogic_vector(to_unsigned(byte_count-1, 6));
+      return ret;
+    else
+      ret(0) := "10" & std_ulogic_vector(to_unsigned(byte_count-1, 6));
+      return ret & pad;
+    end if;
+  end function;
+
+  function spi_shift(mosi: nsl_data.bytestream.byte_string; read_miso : boolean)
+    return nsl_data.bytestream.byte_string
+  is
+    variable ret: nsl_data.bytestream.byte_string(0 to 0);
+  begin
+    assert 0 < mosi'length
+      report "Must have MOSI data"
+      severity failure;
+    assert mosi'length <= 64
+      report "MOSI data cannot be more than 64 bytes"
+      severity failure;
+
+    ret(0) := "1" & to_logic(read_miso) & std_ulogic_vector(to_unsigned(mosi'length-1, 6));
+
+    return ret & mosi;
+  end function;
+
+end package body;
