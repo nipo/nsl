1dc6944f6a9cbc1d95f6b53c6409b7af2df97c3f
Nicolas Pouillon <nipo@ssji.net>
2023-12-22 17:10:56 +0100

    nsl_logic: Add mask_merge

--- lib/nsl_logic/logic/logic.pkg.vhd
+++ lib/nsl_logic/logic/logic.pkg.vhd
@@ -7,6 +7,9 @@ package logic is
   function xor_reduce(x : std_ulogic_vector) return std_ulogic;
   function and_reduce(x : std_ulogic_vector) return std_ulogic;
   function or_reduce(x : std_ulogic_vector) return std_ulogic;
+  function mask_merge(for0, for1, sel : std_ulogic_vector) return std_ulogic_vector;
+  function mask_range(width, lsb, msb : natural;
+                      range_bit: std_ulogic := '1') return std_ulogic_vector;
 
 end package logic;
 
@@ -66,5 +69,46 @@ package body logic is
 
     return ret;
   end or_reduce;
+
+  function mask_merge(for0, for1, sel : std_ulogic_vector) return std_ulogic_vector is
+    variable ret: std_ulogic_vector(sel'range);
+  begin
+    assert for0'length = for1'length
+      report "Input vectors must have the same length"
+      severity failure;
+    assert for0'length = sel'length
+      report "Input vectors and selector must have the same length"
+      severity failure;
+
+    ret := (for0 and not sel) or (for1 and sel);
+
+    return ret;
+  end mask_merge;
+
+  function mask_range(width, lsb, msb : natural;
+                      range_bit: std_ulogic := '1') return std_ulogic_vector is
+    variable ret: std_ulogic_vector(width - 1 downto 0);
+  begin
+    assert lsb < width
+      report "Invalid lsb index"
+      severity failure;
+    assert msb < width
+      report "Invalid msb index"
+      severity failure;
+    assert lsb <= msb
+      report "range in bad order"
+      severity failure;
+
+    for i in ret'range
+    loop
+      if lsb <= i and i <= msb then
+        ret(i) := range_bit;
+      else
+        ret(i) := not range_bit;
+      end if;
+    end loop;
+
+    return ret;
+  end function;
 
 end package body logic;
