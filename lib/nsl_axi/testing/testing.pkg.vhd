a8cdb3dd77ae14f1350ecf40c1052b6c61f6fa51
Nicolas Pouillon <nipo@ssji.net>
2024-02-02 16:54:34 +0100

    nsl_axi/testing: Add an AXI-Lite snooper

--- lib/nsl_axi/testing/testing.pkg.vhd
+++ lib/nsl_axi/testing/testing.pkg.vhd
@@ -1,10 +1,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-library nsl_axi;
+library nsl_axi, nsl_simulation, nsl_data;
+use nsl_simulation.logging.all;
+use nsl_data.text.all;
 
 package testing is
 
+  procedure a32_d32_snooper(constant prefix: string;
+                            signal b: in nsl_axi.axi4_lite.a32_d32;
+                            signal clock: in std_ulogic;
+                            constant clock_period: time);
+                            
+  
   component axis_16l_file_reader is
     generic(
       filename: string
@@ -36,3 +45,38 @@ package testing is
   end component;
 
 end package testing;
+
+package body testing is
+
+  procedure a32_d32_snooper(constant prefix: string;
+                            signal b: in nsl_axi.axi4_lite.a32_d32;
+                            signal clock: in std_ulogic;
+                            constant clock_period: time)
+  is
+    variable waddr, raddr : unsigned(31 downto 0);
+  begin
+    while true
+    loop
+      wait until rising_edge(clock);
+      wait for clock_period * 9 / 10;
+
+      if b.ms.awvalid = '1' and b.sm.awready = '1' then
+        waddr := unsigned(b.ms.awaddr);
+      end if;
+
+      if b.ms.arvalid = '1' and b.sm.arready = '1' then
+        raddr := unsigned(b.ms.araddr);
+      end if;
+
+      if b.ms.wvalid = '1' and b.sm.wready = '1' then
+        log_info(prefix & " W @" & to_string(waddr) & ": " & to_hex_string(b.ms.wdata) & ", strobe: " & to_string(b.ms.wstrb));
+      end if;
+
+      if b.ms.rready = '1' and b.sm.rvalid = '1' then
+        log_info(prefix & " r @" & to_string(raddr) & ": " & to_hex_string(b.sm.rdata) & ", resp: " & to_string(b.sm.rresp));
+      end if;
+
+    end loop;
+  end procedure;
+
+end package body;
