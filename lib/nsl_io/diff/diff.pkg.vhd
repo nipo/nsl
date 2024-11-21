0c4adb2b0ef643e04f84d11d96d31952943f5d5a
Nicolas Pouillon <nipo@ssji.net>
2024-11-21 14:29:54 +0100

    io/diff: Add diff to logic converters

--- lib/nsl_io/diff/diff.pkg.vhd
+++ lib/nsl_io/diff/diff.pkg.vhd
@@ -39,6 +39,22 @@ package diff is
   function from_ppnn(v: std_ulogic_vector;
                      swap_mask: std_ulogic_vector := "0") return diff_pair_vector;
 
+  -- Takes a vector of diff pairs and map them to twice the number of wires.
+  -- Input vector direction is unimportant.
+  -- Maps output in the same order, with P on the left, N on the right.
+  -- Optionally, pairs may be swapped. Swap mask is taken in same order as the
+  -- vector, if shorter, no swap is assumed.
+  function to_pnpn(v: diff_pair_vector;
+                   swap_mask: std_ulogic_vector := "0") return std_ulogic_vector;
+
+  -- Takes a vector of diff pairs and map them to twice the number of
+  -- wires.  Input vector direction is unimportant.  Maps output in
+  -- the same order, with all Ps in order on the left, all Ns on the
+  -- right.  Optionally, pairs may be swapped. Swap mask is taken in
+  -- same order as the vector, if shorter, no swap is assumed.
+  function to_ppnn(v: diff_pair_vector;
+                   swap_mask: std_ulogic_vector := "0") return std_ulogic_vector;
+
 end package diff;
 
 package body diff is
@@ -148,4 +164,46 @@ package body diff is
     return ret;
   end function;
   
+  function to_pnpn(v: diff_pair_vector;
+                   swap_mask: std_ulogic_vector := "0") return std_ulogic_vector
+  is
+    alias vd: diff_pair_vector(0 to v'length-1) is v;
+    variable ret : std_ulogic_vector(0 to v'length*2-1);
+    variable inv: std_ulogic_vector(0 to v'length-1) := vector_extend(swap_mask, v'length);
+  begin
+    for i in vd'range
+    loop
+      if inv(i) = '1' then
+        ret(i*2 + 0) := vd(i).p;
+        ret(i*2 + 1) := vd(i).n;
+      else
+        ret(i*2 + 0) := vd(i).n;
+        ret(i*2 + 1) := vd(i).p;
+      end if;
+    end loop;
+
+    return ret;
+  end function;
+
+  function to_ppnn(v: diff_pair_vector;
+                   swap_mask: std_ulogic_vector := "0") return std_ulogic_vector
+  is
+    alias vd: diff_pair_vector(0 to v'length-1) is v;
+    variable ret : std_ulogic_vector(0 to v'length*2-1);
+    variable inv: std_ulogic_vector(0 to v'length-1) := vector_extend(swap_mask, v'length);
+  begin
+    for i in vd'range
+    loop
+      if inv(i) = '1' then
+        ret(i) := vd(i).p;
+        ret(i + vd'length) := vd(i).n;
+      else
+        ret(i) := vd(i).n;
+        ret(i + vd'length) := vd(i).p;
+      end if;
+    end loop;
+
+    return ret;
+  end function;
+
 end package body diff;
