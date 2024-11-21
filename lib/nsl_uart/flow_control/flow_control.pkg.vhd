99b4715d3e48be1a80ecd8c803ca3cb9f52c615a
Nicolas Pouillon <nipo@ssji.net>
2024-11-21 14:40:42 +0100

    uart: Update flow control implementation
    This was actually broken.

--- lib/nsl_uart/flow_control/flow_control.pkg.vhd
+++ lib/nsl_uart/flow_control/flow_control.pkg.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library nsl_data;
+library nsl_data, nsl_bnoc;
 use nsl_data.bytestream.all;
 
 package flow_control is
@@ -10,43 +10,52 @@ package flow_control is
   component xonxoff_rx is
     generic(
       xoff_c: byte := x"13";
-      xon_c: byte := x"11"
+      xon_c: byte := x"11";
+      extra_rx_depth_c : natural := 2
       );
     port(
       clock_i     : in std_ulogic;
       reset_n_i   : in std_ulogic;
 
-      ready_o : out std_ulogic;
-      
-      serdes_data_i  : in byte;
-      serdes_valid_i : in std_ulogic;
-      serdes_ready_o : out std_ulogic
+      -- Enable flow control, if not enabled, assume TX and RX are
+      -- infinite
+      enable_i : in std_ulogic := '1';
 
-      rx_data_o      : out byte;
-      rx_valid_o     : out std_ulogic;
-      rx_ready_i     : in std_ulogic
+      peer_ready_o : out std_ulogic;
+      rx_ready_o : out std_ulogic;
+      
+      serdes_i : in nsl_bnoc.pipe.pipe_req_t;
+      serdes_o : out nsl_bnoc.pipe.pipe_ack_t;
+
+      rx_o : out nsl_bnoc.pipe.pipe_req_t;
+      rx_i : in nsl_bnoc.pipe.pipe_ack_t
       );
   end component;
 
   component xonxoff_tx is
     generic(
       xoff_c: byte := x"13";
-      xon_c: byte := x"11"
+      xon_c: byte := x"11";
+      refresh_every_c : integer := 0
       );
     port(
       clock_i     : in std_ulogic;
       reset_n_i   : in std_ulogic;
 
-      ready_i     : in std_ulogic;
+      -- Enable flow control transmission and limiting
+      enable_i : in std_ulogic := '1';
 
-      tx_data_i  : in byte;
-      tx_valid_i : in std_ulogic;
-      tx_ready_o : out std_ulogic
+      -- Gates the TX path, controlled by RX side's "Peer ready" signal
+      can_transmit_i : in std_ulogic := '1';
+      -- Tells whether to send XON or XOFF
+      can_receive_i : in std_ulogic := '1';
 
-      serdes_data_o      : out byte;
-      serdes_valid_o     : out std_ulogic;
-      serdes_ready_i     : in std_ulogic
+      tx_i : in nsl_bnoc.pipe.pipe_req_t;
+      tx_o : out nsl_bnoc.pipe.pipe_ack_t;
+
+      serdes_o : out nsl_bnoc.pipe.pipe_req_t;
+      serdes_i : in nsl_bnoc.pipe.pipe_ack_t
       );
   end component;
 
-end package ssp;
+end package flow_control;
--- lib/nsl_uart/flow_control/xonxoff_tx.vhd
+++ lib/nsl_uart/flow_control/xonxoff_tx.vhd
@@ -2,27 +2,30 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library nsl_data;
+library nsl_data, nsl_bnoc;
+use nsl_bnoc.pipe.all;
 use nsl_data.bytestream.all;
 
 entity xonxoff_tx is
   generic(
     xoff_c: byte := x"13";
-    xon_c: byte := x"11"
+    xon_c: byte := x"11";
+    refresh_every_c : integer := 0
     );
   port(
     clock_i     : in std_ulogic;
     reset_n_i   : in std_ulogic;
 
-    ready_i     : in std_ulogic;
+    enable_i : in std_ulogic := '1';
 
-    tx_data_i  : in byte;
-    tx_valid_i : in std_ulogic;
-    tx_ready_o : out std_ulogic
+    can_transmit_i : in std_ulogic := '1';
+    can_receive_i : in std_ulogic := '1';
 
-    serdes_data_o      : out byte;
-    serdes_valid_o     : out std_ulogic;
-    serdes_ready_i     : in std_ulogic
+    tx_i : in pipe_req_t;
+    tx_o : out pipe_ack_t;
+
+    serdes_o : out pipe_req_t;
+    serdes_i : in pipe_ack_t
     );
 end entity;
 
@@ -34,8 +37,10 @@ architecture beh of xonxoff_tx is
   record
     fifo: byte_string(0 to fifo_depth_c-1);
     fifo_fillness: integer range 0 to fifo_depth_c;
-    ready: std_ulogic;
-    ready_changed: boolean;
+    can_transmit: boolean;
+    can_receive: boolean;
+    can_receive_changed: boolean;
+    can_receive_refresh: integer range 0 to refresh_every_c;
   end record;
 
   signal r, rin: regs_t;
@@ -44,14 +49,20 @@ begin
 
   regs: process(reset_n_i, clock_i) is
   begin
+    if rising_edge(clock_i) then
+      r <= rin;
+    end if;
+
     if reset_n_i = '0' then
       r.fifo_fillness <= 0;
-      r.ready <= '1';
-      r.ready_changed <= false;
+      r.can_transmit <= false;
+      r.can_receive <= true;
+      r.can_receive_changed <= false;
+      r.can_receive_refresh <= refresh_every_c;
     end if;
   end process;
 
-  transition: process(r, serdes_ready_i, tx_data_i, tx_valid_i, ready_i) is
+  transition: process(r, serdes_i, tx_i, tx_i, can_receive_i, can_transmit_i, enable_i) is
     variable fifo_push, fifo_pop: boolean;
   begin
     rin <= r;
@@ -59,53 +70,69 @@ begin
     fifo_pop := false;
     fifo_push := false;
 
-    if serdes_ready_i = '1' then
-      if r.ready_changed then
-        rin.ready_changed <= false;
-      elsif r.fifo_fillness /= 0 then
+    rin.can_transmit <= can_transmit_i = '1';
+
+    if serdes_i.ready = '1' then
+      if r.can_receive_changed then
+        rin.can_receive_changed <= false;
+      elsif r.fifo_fillness /= 0 and r.can_transmit then
         fifo_pop := true;
       end if;
     end if;
 
-    if r.fifo_fillness < fifo_depth_c and tx_valid_i = '1' then
+    if refresh_every_c /= 0 then
+      if tx_i.valid = '1' then
+        rin.can_receive_refresh <= refresh_every_c;
+      elsif r.can_receive_changed then
+        rin.can_receive_refresh <= refresh_every_c;
+      elsif r.can_receive_refresh /= 0 then
+        rin.can_receive_refresh <= r.can_receive_refresh - 1;
+      else
+        rin.can_receive_changed <= true;
+      end if;
+    end if;
+
+    if r.fifo_fillness < fifo_depth_c and tx_i.valid = '1' then
       fifo_push := true;
     end if;
     
     if fifo_push and fifo_pop then
       rin.fifo <= shift_left(r.fifo);
-      rin.fifo(r.fifo_fillness-1) <= tx_data_i;
+      rin.fifo(r.fifo_fillness-1) <= tx_i.data;
     elsif fifo_push then
-      rin.fifo(r.fifo_fillness) <= tx_data_i;
+      rin.fifo(r.fifo_fillness) <= tx_i.data;
       rin.fifo_fillness <= r.fifo_fillness + 1;
     elsif fifo_pop then
       rin.fifo <= shift_left(r.fifo);
       rin.fifo_fillness <= r.fifo_fillness - 1;
     end if;
 
-    if ready_i /= r.ready then
-      rin.ready_changed <= true;
-      rin.ready <= ready_i;
+    if (can_receive_i = '1') /= r.can_receive then
+      rin.can_receive_changed <= true;
+      rin.can_receive <= can_receive_i = '1';
+    end if;
+
+    if enable_i = '0' then
+      rin.can_receive_changed <= false;
+      rin.can_receive <= true;
+      rin.can_transmit <= true;
+      rin.can_receive_refresh <= refresh_every_c;
     end if;
   end process;
 
   moore: process(r) is
   begin
-    tx_ready_o <= '0';
-    if r.fifo_fillness < fifo_depth_c then
-      tx_ready_o <= '1';
-    end if;
+    tx_o <= pipe_accept(r.fifo_fillness < fifo_depth_c);
+    serdes_o <= pipe_req_idle_c;
 
-    serdes_valid_o <= '0';
-    if r.ready_changed then
-      serdes_valid_o <= '1';
-      if r.ready = '1' then
-        serdes_data_o <= xon_c;
+    if r.can_receive_changed then
+      if r.can_receive then
+        serdes_o <= pipe_flit(xon_c);
       else
-        serdes_data_o <= xoff_c;
+        serdes_o <= pipe_flit(xoff_c);
       end if;
-    elsif r.fifo_fillness /= 0 then
-      serdes_valid_o <= '1';
-      serdes_data_o <= r.fifo(0);
+    elsif r.fifo_fillness /= 0 and r.can_transmit then
+      serdes_o <= pipe_flit(r.fifo(0));
     end if;
   end process;
 
