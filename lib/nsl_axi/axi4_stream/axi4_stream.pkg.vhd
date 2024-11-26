c420f8157b0c614b7869a36991f0259efb36f95c
Nicolas Pouillon <nipo@ssji.net>
2024-11-26 22:40:48 +0100

    axi/axi4_stream: Add test-bench utilies for send/receive

--- lib/nsl_axi/axi4_stream/axi4_stream.pkg.vhd
+++ lib/nsl_axi/axi4_stream/axi4_stream.pkg.vhd
@@ -211,6 +211,34 @@ package axi4_stream is
   function to_string(cfg: config_t; a: master_t) return string;
   function to_string(cfg: config_t; a: slave_t) return string;
 
+  -- Simulation helper function to issue a write transaction to an
+  -- AXI4-Stream bus.
+  procedure send(constant cfg: config_t;
+                 signal clock: in std_ulogic;
+                 signal stream_i: in slave_t;
+                 signal stream_o: out master_t;
+                 constant beat: master_t);
+
+  procedure send(constant cfg: config_t;
+                 signal clock: in std_ulogic;
+                 signal stream_i: in slave_t;
+                 signal stream_o: out master_t;
+                 constant bytes: byte_string;
+                 constant strobe: std_ulogic_vector := na_suv;
+                 constant keep: std_ulogic_vector := na_suv;
+                 constant endian: endian_t := ENDIAN_LITTLE;
+                 constant id: std_ulogic_vector := na_suv;
+                 constant user: std_ulogic_vector := na_suv;
+                 constant dest: std_ulogic_vector := na_suv;
+                 constant valid : boolean := true;
+                 constant last : boolean := false);
+
+  procedure receive(constant cfg: config_t;
+                    signal clock: in std_ulogic;
+                    signal stream_i: in master_t;
+                    signal stream_o: out slave_t;
+                    variable beat: out master_t);
+
 end package;
 
 package body axi4_stream is
@@ -651,5 +679,80 @@ package body axi4_stream is
       &">";
   end function;
 
-end package body axi4_stream;
+  procedure send(constant cfg: config_t;
+                 signal clock: in std_ulogic;
+                 signal stream_i: in slave_t;
+                 signal stream_o: out master_t;
+                 constant beat: master_t)
+  is
+    variable done: boolean := false;
+  begin
+    assert is_valid(cfg, beat)
+      report "Cannot send a non-valid beat"
+      severity failure;
+    
+    stream_o <= beat;
+    while not done
+      loop
+      wait until rising_edge(clock);
+      done := is_ready(cfg, stream_i);
 
+      wait until falling_edge(clock);
+    end loop;
+
+    stream_o <= transfer_defaults(cfg);
+  end procedure;
+
+  procedure send(constant cfg: config_t;
+                 signal clock: in std_ulogic;
+                 signal stream_i: in slave_t;
+                 signal stream_o: out master_t;
+                 constant bytes: byte_string;
+                 constant strobe: std_ulogic_vector := na_suv;
+                 constant keep: std_ulogic_vector := na_suv;
+                 constant endian: endian_t := ENDIAN_LITTLE;
+                 constant id: std_ulogic_vector := na_suv;
+                 constant user: std_ulogic_vector := na_suv;
+                 constant dest: std_ulogic_vector := na_suv;
+                 constant valid : boolean := true;
+                 constant last : boolean := false)
+  is
+  begin
+    send(cfg, clock, stream_i, stream_o, transfer(cfg,
+                                                  bytes => bytes,
+                                                  strobe => strobe,
+                                                  keep => keep,
+                                                  endian => endian,
+                                                  id => id,
+                                                  user => user,
+                                                  dest => dest,
+                                                  valid => valid,
+                                                  last => last));
+  end procedure;
+
+  procedure receive(constant cfg: config_t;
+                    signal clock: in std_ulogic;
+                    signal stream_i: in master_t;
+                    signal stream_o: out slave_t;
+                    variable beat: out master_t)
+  is
+    variable done: boolean := false;
+  begin
+    stream_o <= accept(cfg, true);
+    
+    while not done
+    loop
+      wait until rising_edge(clock);
+      if is_valid(cfg, stream_i) then
+        done := true;
+        beat := stream_i;
+      end if;
+
+      wait until falling_edge(clock);
+      if done then
+        stream_o <= accept(cfg, false);
+      end if;
+    end loop;
+  end procedure;
+
+end package body axi4_stream;
