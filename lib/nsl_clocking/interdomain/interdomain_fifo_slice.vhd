995c2232184f1beee252b5bbeab7bc1e96a04134
Nicolas Pouillon <nipo@ssji.net>
2024-12-07 18:16:58 +0100

    clocking/interdomain_fifo_slice: Fix first cycle after reset

--- lib/nsl_clocking/interdomain/interdomain_fifo_slice.vhd
+++ lib/nsl_clocking/interdomain/interdomain_fifo_slice.vhd
@@ -23,23 +23,13 @@ end entity;
 
 architecture beh of interdomain_fifo_slice is
 
-  type in_regs_t is
-  record
-    sn : std_ulogic;
-    data : std_ulogic_vector(data_width_c-1 downto 0);
-  end record;
+  subtype data_t is std_ulogic_vector(data_width_c-1 downto 0);
   
-  signal in_r, in_rin, out_sin : in_regs_t;
-
-  signal ito_in, ito_out : std_ulogic_vector(data_width_c downto 0);
+  signal i2o_data_in_s, i2o_data_out_s : data_t;
+  signal i2o_toggle_in_s, i2o_toggle_out_s : std_ulogic;
+  signal o2i_toggle_in_s, o2i_toggle_out_s : std_ulogic;
   
-  type out_regs_t is
-  record
-    nesn : std_ulogic;
-  end record;
-  
-  signal out_r, out_rin, in_sout : out_regs_t;
-  signal s_resetn : std_ulogic_vector(0 to 1);
+  signal reset_n_s : std_ulogic_vector(0 to 1);
 
 begin
 
@@ -51,76 +41,154 @@ begin
     port map(
       clock_i => clock_i,
       master_i => reset_n_i,
-      slave_o => s_resetn
+      slave_o => reset_n_s
       );
 
-  in_regs: process(clock_i(0), s_resetn(0))
-  begin
-    if s_resetn(0) = '0' then
-      in_r.sn <= '0';
-    elsif not clock_i(0)'stable and clock_i(0) = '1' then
-      in_r <= in_rin;
-    end if;
-  end process;
-
-  out_regs: process(clock_i(1), s_resetn(1))
-  begin
-    if s_resetn(1) = '0' then
-      out_r.nesn <= '1';
-    elsif not clock_i(1)'stable and clock_i(1) = '1' then
-      out_r <= out_rin;
-    end if;
-  end process;
-
-  in_transition: process(in_data_i, in_valid_i, in_r, in_sout)
-  begin
-    in_rin <= in_r;
-
-    if in_r.sn /= in_sout.nesn and in_valid_i = '1' then
-      in_rin.data <= in_data_i;
-      in_rin.sn <= not in_r.sn;
-    end if;
-  end process;
-
-  in_ready_o <= '1' when in_r.sn /= in_sout.nesn else '0';
-
-  out_transition: process(out_ready_i, out_r, out_sin)
-  begin
-    out_rin <= out_r;
-
-    if out_sin.sn = out_r.nesn and out_ready_i = '1' then
-      out_rin.nesn <= not out_r.nesn;
-    end if;
-  end process;
-
-  out_valid_o <= '1' when out_sin.sn = out_r.nesn else '0';
-  out_data_o <= out_sin.data;
-
-  in_to_out_sn: nsl_clocking.interdomain.interdomain_reg
-    generic map(
-      stable_count_c => 1,
-      cycle_count_c => 2,
-      data_width_c => 1 + data_width_c
-      )
-    port map(
-      clock_i => clock_i(1),
-      data_i => ito_in,
-      data_o => ito_out
+  in_side: block is
+    type state_t is (
+      ST_RESET,
+      ST_IDLE,
+      ST_WAIT
       );
+
+    type regs_t is
+    record
+      state : state_t;
+      toggle: std_ulogic;
+      data : data_t;
+    end record;
+
+    signal r, rin : regs_t;
+  begin
+    regs: process(clock_i(0), reset_n_s(0))
+    begin
+      if rising_edge(clock_i(0)) then
+        r <= rin;
+      end if;
+
+      if reset_n_s(0) = '0' then
+        r.state <= ST_RESET;
+        r.toggle <= '0';
+      end if;
+    end process;
+
+    transition: process(r, in_data_i, in_valid_i, o2i_toggle_in_s)
+    begin
+      rin <= r;
+
+      case r.state is
+        when ST_RESET =>
+          rin.state <= ST_IDLE;
+
+        when ST_IDLE =>
+          if in_valid_i = '1' then
+            rin.state <= ST_WAIT;
+            rin.data <= in_data_i;
+            rin.toggle <= not r.toggle;
+          end if;
+
+        when ST_WAIT =>
+          if o2i_toggle_in_s = r.toggle then
+            rin.state <= ST_IDLE;
+          end if;
+      end case;
+    end process;
+
+    i2o_data_in_s <= r.data;
+    i2o_toggle_in_s <= r.toggle;
+    in_ready_o <= '1' when r.state = ST_IDLE else '0';
+  end block;
+
+  in2out: block is
+  begin
+    toggle: nsl_clocking.interdomain.interdomain_reg
+      generic map(
+        cycle_count_c => 2,
+        data_width_c => 1
+        )
+      port map(
+        clock_i => clock_i(1),
+        data_i(0) => i2o_toggle_in_s,
+        data_o(0) => i2o_toggle_out_s
+        );
+
+    data: nsl_clocking.async.async_sampler
+      generic map(
+        data_width_c => data_t'length
+        )
+      port map(
+        clock_i => clock_i(1),
+        data_i => i2o_data_in_s,
+        data_o => i2o_data_out_s
+        );
+  end block;
+
+  out2in: block is
+  begin
+    toggle: nsl_clocking.interdomain.interdomain_reg
+      generic map(
+        cycle_count_c => 2,
+        data_width_c => 1
+        )
+      port map(
+        clock_i => clock_i(0),
+        data_i(0) => o2i_toggle_out_s,
+        data_o(0) => o2i_toggle_in_s
+        );
+  end block;
   
-  ito_in <= in_r.sn & in_r.data;
-  out_sin.data <= ito_out(out_sin.data'range);
-  out_sin.sn <= ito_out(out_sin.data'length);
-
-  out_to_in: nsl_clocking.interdomain.interdomain_reg
-    generic map(
-      cycle_count_c => 2,
-      data_width_c => 1
-      )
-    port map(
-      clock_i => clock_i(0),
-      data_i(0) => out_r.nesn,
-      data_o(0) => in_sout.nesn
+  out_side: block is
+    type state_t is (
+      ST_RESET,
+      ST_IDLE,
+      ST_WAIT
       );
-  
+
+    type regs_t is
+    record
+      state : state_t;
+      toggle : std_ulogic;
+      data : data_t;
+    end record;
+    
+    signal r, rin: regs_t;
+  begin
+    regs: process(clock_i(1), reset_n_s(1))
+    begin
+      if rising_edge(clock_i(1)) then
+        r <= rin;
+      end if;
+
+      if reset_n_s(1) = '0' then
+        r.state <= ST_RESET;
+        r.toggle <= '0';
+      end if;
+    end process;
+
+    transition: process(r, i2o_data_out_s, i2o_toggle_out_s, out_ready_i)
+    begin
+      rin <= r;
+
+      case r.state is
+        when ST_RESET =>
+          rin.state <= ST_IDLE;
+
+        when ST_IDLE =>
+          if i2o_toggle_out_s /= r.toggle then
+            rin.state <= ST_WAIT;
+            rin.data <= i2o_data_out_s;
+          end if;
+
+        when ST_WAIT =>
+          if out_ready_i = '1' then
+            rin.state <= ST_IDLE;
+            rin.toggle <= not r.toggle;
+          end if;
+      end case;
+    end process;
+
+    o2i_toggle_out_s <= r.toggle;
+    out_valid_o <= '1' when r.state = ST_WAIT else '0';
+    out_data_o <= r.data;
+  end block;
 end architecture;
