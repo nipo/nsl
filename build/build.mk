0e7e815af0bf962de765b6a11c5be622e4e5336f
Thibault Trombert <thibault.trombertv3@gmail.com>
2025-07-29 17:11:01 +0200

    Small fixes in .mk

--- build/build.mk
+++ build/build.mk
@@ -6,7 +6,7 @@ target-usage ?= synthesis
 .SUFFIXES:
 
 SRC_DIR := $(shell cd $(shell pwd) ; cd $(dir $(firstword $(MAKEFILE_LIST))) ; pwd)
-build-dir := $(SRC_DIR)/$(tool)-build
+build-dir = $(SRC_DIR)/$(tool)-build
 
 BUILD_ROOT := $(shell cd $(shell pwd) ; cd $(dir $(lastword $(MAKEFILE_LIST))) ; pwd)
 NSL_ROOT := $(shell cd $(BUILD_ROOT) ; cd ../lib ; pwd)
