4ec5b6fa61f08b24cf43eb4edc5a07ba13261514
Nicolas Pouillon <nipo@ssji.net>
2025-09-06 11:38:47 +0200

    hwdep: Add GW5A-25A config

---
+++ lib/nsl_hwdep/gowin_config/gowin_config_GW5A-25A.pkg.vhd
@@ -0,0 +1,51 @@
+package body gowin_config is
+
+  function iodelay_step_ps return real
+  is
+  begin
+    return 12.5;
+  end function;
+
+  function device_name return string
+  is
+  begin
+    return "GW5A-25A";
+  end function;
+
+  function internal_osc return string
+  is
+  begin
+    return "osca";
+  end function;
+
+  function pll_type return string
+  is
+  begin
+    return "pll";
+  end function;
+
+  function pll_vco_fmin return real
+  is
+  begin
+    return 650.0e6;
+  end function;
+
+  function pll_vco_fmax return real
+  is
+  begin
+    return 1300.0e6;
+  end function;
+
+  function pll_pfd_fmin return real
+  is
+  begin
+    return 19.0e6;
+  end function;
+
+  function pll_pfd_fmax return real
+  is
+  begin
+    return 81.25e6;
+  end function;
+
+end package body;
