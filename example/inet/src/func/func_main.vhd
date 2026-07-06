24e274938ec65c811536a6bb48ce2aac0efd007e
Nicolas Pouillon <nipo@ssji.net>
2026-07-06 07:09:26 +0200

    example/inet: Use function block from library

--- example/inet/src/func/func_main.vhd
+++ example/inet/src/func/func_main.vhd
@@ -8,7 +8,8 @@ use nsl_inet.ethernet.all;
 use nsl_inet.ipv4.all;
 use nsl_inet.udp.all;
 use nsl_data.bytestream.all;
-
+use nsl_bnoc.committed.all;
+  
 entity func_main is
   generic(
     clock_hz_c : integer
@@ -36,98 +37,61 @@ architecture arch of func_main is
 
   constant ethertype_list_c : ethertype_vector(0 to 1) := (0 => ethertype_ipv4,
                                                            1 => ethertype_arp);
-  constant ip_proto_list_c: ip_proto_vector(0 to 0) := (0 => ip_proto_udp);
-  
-  signal l2_to_ipv4_s, ipv4_to_l2_s, l2_to_arp_s, arp_to_l2_s,
-    l3_to_udp_s, udp_to_l3_s, udp_loopback_s: nsl_bnoc.committed.committed_bus;
-  signal s_frame_tx, s_frame_rx: nsl_bnoc.framed.framed_bus;
-
-  signal reset_n : std_ulogic;
 
   constant local_mac_c : mac48_t := from_hex("02deadbeef01");
   constant local_ipv4_c : ipv4_t := to_ipv4(10,0,5,1);
   constant netmask_ipv4_c : ipv4_t := to_ipv4(255,255,255,0);
   constant broadcast_ipv4_c : ipv4_t := to_ipv4(10,0,5,255);
   constant gateway_ipv4_c : ipv4_t := to_ipv4(10,0,5,254);
-  
+
+  type committed_trx_t is
+  record
+    rx, tx: committed_bus_t;
+  end record;
+
+  signal udp_loopback_s: committed_trx_t;
+
 begin
-  
-  eth: nsl_inet.ethernet.ethernet_layer
+
+  host: nsl_inet.func.ethernet_host
     generic map(
-      ethertype_c => ethertype_list_c,
-      l1_header_length_c => 0
+      clock_hz_c => clock_hz_c,
+      udp_port_c => (0 => 1234)
       )
     port map(
       clock_i => clock_i,
       reset_n_i => reset_n_i,
 
-      local_address_i => local_mac_c,
-
-      to_l1_o => net_to_l1_o,
-      to_l1_i => net_to_l1_i,
-      from_l1_i => net_from_l1_i,
-      from_l1_o => net_from_l1_o,
-
-      to_l3_o(0) => l2_to_ipv4_s.req,
-      to_l3_o(1) => l2_to_arp_s.req,
-      to_l3_i(0) => l2_to_ipv4_s.ack,
-      to_l3_i(1) => l2_to_arp_s.ack,
-      from_l3_i(0) => ipv4_to_l2_s.req,
-      from_l3_i(1) => arp_to_l2_s.req,
-      from_l3_o(0) => ipv4_to_l2_s.ack,
-      from_l3_o(1) => arp_to_l2_s.ack
-      );
-
-  ipv4: nsl_inet.ipv4.ipv4_layer_ethernet
-    generic map(
-      l1_header_length_c => 0,
-      ip_proto_c => ip_proto_list_c,
-      clock_i_hz_c => clock_hz_c
-      )
-    port map(
-      reset_n_i => reset_n_i,
-      clock_i => clock_i,
-
-      unicast_i => local_ipv4_c,
-      netmask_i => netmask_ipv4_c,
-      gateway_i => gateway_ipv4_c,
       hwaddr_i => local_mac_c,
 
-      to_l4_o(0) => l3_to_udp_s.req,
-      to_l4_i(0) => l3_to_udp_s.ack,
-      from_l4_i(0) => udp_to_l3_s.req,
-      from_l4_o(0) => udp_to_l3_s.ack,
+      unicast_i => local_ipv4_c,
+      gateway_i => gateway_ipv4_c,
+      netmask_i => netmask_ipv4_c,
+      broadcast_i => broadcast_ipv4_c,
 
-      ip_to_l2_o => ipv4_to_l2_s.req,
-      ip_to_l2_i => ipv4_to_l2_s.ack,
-      ip_from_l2_i => l2_to_ipv4_s.req,
-      ip_from_l2_o => l2_to_ipv4_s.ack,
+      l1_tx_o => net_to_l1_o,
+      l1_tx_i => net_to_l1_i,
+      l1_rx_i => net_from_l1_i,
+      l1_rx_o => net_from_l1_o,
 
-      arp_to_l2_o => arp_to_l2_s.req,
-      arp_to_l2_i => arp_to_l2_s.ack,
-      arp_from_l2_i => l2_to_arp_s.req,
-      arp_from_l2_o => l2_to_arp_s.ack
+      udp_rx_o(0) => udp_loopback_s.rx.req,
+      udp_rx_i(0) => udp_loopback_s.rx.ack,
+      udp_tx_i(0) => udp_loopback_s.tx.req,
+      udp_tx_o(0) => udp_loopback_s.tx.ack
       );
 
-  udp: nsl_inet.udp.udp_layer
+  udp_fifo: nsl_bnoc.committed.committed_fifo
     generic map(
-      tx_mtu_c => 1500,
-      udp_port_c => (0 => 1234),
-      header_length_c => ipv4_header_length_c
+      depth_c => 2048
       )
     port map(
       reset_n_i => reset_n_i,
-      clock_i => clock_i,
+      clock_i(0) => clock_i,
 
-      from_L3_i => l3_to_udp_s.req,
-      from_L3_o => l3_to_udp_s.ack,
-      to_L3_o => udp_to_l3_s.req,
-      to_L3_i => udp_to_l3_s.ack,
-
-      to_l5_o(0) => udp_loopback_s.req,
-      to_l5_i(0) => udp_loopback_s.ack,
-      from_l5_i(0) => udp_loopback_s.req,
-      from_l5_o(0) => udp_loopback_s.ack
+      in_i => udp_loopback_s.rx.req,
+      in_o => udp_loopback_s.rx.ack,
+      out_o => udp_loopback_s.tx.req,
+      out_i => udp_loopback_s.tx.ack
       );
   
   led_o(0 to 2) <= button_i(0 to 2);
@@ -139,7 +103,7 @@ begin
     port map(
       reset_n_i => reset_n_i,
       clock_i => clock_i,
-      activity_i => udp_loopback_s.req.valid,
+      activity_i => udp_loopback_s.rx.req.valid,
       led_o => led_o(3)
       );
 
