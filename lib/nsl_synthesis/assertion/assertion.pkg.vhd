888f201d092a886fa99aaf1ea9daf43bf74b9124
wet-in-the-kamp <isaac.wetenkamp@ellisys.com>
2026-09-04 13:45:37 +0200

    nsl_synthesis/assertion: Use procedure-based failure for more flexibility (can now use in functions), add test bench

--- lib/nsl_synthesis/assertion/assertion.pkg.vhd
+++ lib/nsl_synthesis/assertion/assertion.pkg.vhd
@@ -3,17 +3,30 @@ use ieee.std_logic_1164.all;
 
 package assertion is
 
-  -- Hacky component for failing elaboration if condition is not true.
-  --
-  -- Apart from this, this component has no use.
-  component synth_assert is
-    generic(
-      message_c: string;
-      condition_c: boolean
-      );
-    port(
-      unused_i : in std_ulogic
-      );
-  end component;
+  -- Hacky procedure for failing elaboration if condition is not true.
+  procedure synth_assert(constant condition_c : in boolean);
+  
+  function resolve(c: boolean) return integer;
   
 end package;
+
+package body assertion is
+
+  function resolve(c: boolean) return integer
+  is
+  begin
+    if c then
+      return 0;
+    else
+      return 1;
+    end if;
+  end function;  
+
+  procedure synth_assert(constant condition_c : in boolean)
+  is
+    variable assert_fail: std_ulogic_vector(0 to 0);
+  begin
+    assert_fail(resolve(condition_c)) := '-';
+  end procedure;
+
+end package body;
