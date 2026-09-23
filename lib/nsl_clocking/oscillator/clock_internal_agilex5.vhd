c4f4f91fe50f1726b14f9ac09785620480084b50
Nicolas Pouillon <nipo@ssji.net>
2026-09-23 16:05:54 +0200

    clocking/oscillator: Take the Agilex 5 oscillator from its SDM atom

--- lib/nsl_clocking/oscillator/clock_internal_agilex5.vhd
+++ lib/nsl_clocking/oscillator/clock_internal_agilex5.vhd
@@ -1,28 +1,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+-- Agilex 5 internal oscillator, the one the SDM runs from.  The data
+-- sheet publishes no rate for it on the fabric side.
 entity clock_internal is
   port(
     clock_o      : out std_ulogic
     );
 end entity;
 
-architecture efinix of clock_internal is
+architecture agilex5 of clock_internal is
 
-  signal int_clk : std_ulogic;
-
-  component fourteennm_sdm_oscillator is
+  component tennm_sdm_oscillator is
     port(
-      clkout : out std_ulogic;
-      clkout1 : out std_ulogic
+      clkout : out std_logic;
+      clkout1 : out std_logic
       );
   end component;
-  
+
 begin
 
-  gen: fourteennm_sdm_oscillator
+  gen: tennm_sdm_oscillator
     port map(
-      clkout => clock_o
+      clkout => clock_o,
+      clkout1 => open
       );
-  
+
 end architecture;
